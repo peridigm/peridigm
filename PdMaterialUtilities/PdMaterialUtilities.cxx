77a898cb0bfafda6ed490abbbce5bef9bdef3e3d
Michael Parks <mlparks@sandia.gov>
2010-08-10 17:33:34 +0000

    Restructuring
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@384 d9297710-12f9-482b-b11f-fb677dc88bbb

---
+++ PdMaterialUtilities/PdMaterialUtilities.cxx
@@ -0,0 +1,420 @@
+/*
+ * PdMaterialUtilities.cxx
+ *
+ */
+#include "PdMaterialUtilities.h"
+#include <math.h>
+#include <iostream>
+
+namespace PdMaterialUtilities {
+
+
+void updateGeometry
+(
+		const double* xOverlap,
+		const double* uOverlap,
+		const double* velocityOverlap,
+		double* yOverlap,
+		int overLapLength,
+		double dt
+)
+{
+	const double* x = xOverlap;
+	const double* u = uOverlap;
+	const double* v = velocityOverlap;
+	double*       y = yOverlap;
+
+	int length = overLapLength;
+	for(;x!=xOverlap+length;x++,u++,v++,y++)
+		*y = *x + *u + *v * dt;
+}
+
+void computeWeightedVolume
+(
+		const double* xOverlap,
+		const double* volumeOverlap,
+		double *mOwned,
+		int myNumPoints,
+		const int* localNeighborList
+){
+	double *m = mOwned;
+	const double *xOwned = xOverlap;
+	double cellVolume;
+	const int *neighPtr = localNeighborList;
+	for(int p=0;p<myNumPoints;p++, xOwned+=3, m++){
+		int numNeigh = *neighPtr; neighPtr++;
+		const double *X = xOwned;
+		*m=0;
+		for(int n=0;n<numNeigh;n++,neighPtr++){
+			int localId = *neighPtr;
+			cellVolume = volumeOverlap[localId];
+			const double *XP = &xOverlap[3*localId];
+			double dx = XP[0]-X[0];
+			double dy = XP[1]-X[1];
+			double dz = XP[2]-X[2];
+			*m+=(dx*dx+dy*dy+dz*dz)*cellVolume;
+		}
+	}
+}
+
+
+void computeDilatation
+(
+		const double* xOverlap,
+		const double* yOverlap,
+		const double *mOwned,
+		const double* volumeOverlap,
+		const double* bondDamage,
+		double* dilatationOwned,
+		const int* localNeighborList,
+		int numOwnedPoints
+)
+{
+	double OMEGA=1.0;
+	const double *xOwned = xOverlap;
+	const double *yOwned = yOverlap;
+	const double *m = mOwned;
+	const double *v = volumeOverlap;
+	double *theta = dilatationOwned;
+	double cellVolume;
+	const int *neighPtr = localNeighborList;
+	for(int p=0; p<numOwnedPoints;p++, xOwned+=3, yOwned+=3, m++, theta++){
+		int numNeigh = *neighPtr; neighPtr++;
+		const double *X = xOwned;
+		const double *Y = yOwned;
+		*theta = 0;
+		for(int n=0;n<numNeigh;n++,neighPtr++,bondDamage++){
+			int localId = *neighPtr;
+			cellVolume = v[localId];
+			const double *XP = &xOverlap[3*localId];
+			const double *YP = &yOverlap[3*localId];
+			double dx = XP[0]-X[0];
+			double dy = XP[1]-X[1];
+			double dz = XP[2]-X[2];
+			double zetaSqared = dx*dx+dy*dy+dz*dz;
+			dx = YP[0]-Y[0];
+			dy = YP[1]-Y[1];
+			dz = YP[2]-Y[2];
+			double dY = dx*dx+dy*dy+dz*dz;
+			double d = sqrt(zetaSqared);
+			double e = sqrt(dY)-d;
+			*theta += 3.0*OMEGA*(1.0-*bondDamage)*d*e*cellVolume/(*m);
+		}
+
+	}
+}
+
+void computeInternalForceLinearElastic
+(
+		const double* xOverlap,
+		const double* yOverlap,
+		const double* mOwned,
+		const double* volumeOverlap,
+		const double* dilatationOwned,
+		const double* bondDamage,
+		double* fInternalOverlap,
+		const int*  localNeighborList,
+		int numOwnedPoints,
+		double BULK_MODULUS,
+		double SHEAR_MODULUS
+)
+{
+
+	/*
+	 * Compute processor local contribution to internal force
+	 */
+	double K = BULK_MODULUS;
+	double MU = SHEAR_MODULUS;
+	double OMEGA=1.0;
+
+	const double *xOwned = xOverlap;
+	const double *yOwned = yOverlap;
+	const double *m = mOwned;
+	const double *v = volumeOverlap;
+	const double *theta = dilatationOwned;
+	double *fOwned = fInternalOverlap;
+
+	const int *neighPtr = localNeighborList;
+	double cellVolume, alpha, dx, dy, dz, zeta, dY, t;
+	for(int p=0;p<numOwnedPoints;p++, xOwned +=3, yOwned +=3, fOwned+=3, m++, theta++){
+
+		int numNeigh = *neighPtr; neighPtr++;
+		const double *X = xOwned;
+		const double *Y = yOwned;
+		alpha = 15.0*MU/(*m);
+		double selfCellVolume = v[p];
+		double c1 = OMEGA*(*theta)*(9.0*K-15.0*MU)/(3.0*(*m));
+		for(int n=0;n<numNeigh;n++,neighPtr++,bondDamage++){
+			int localId = *neighPtr;
+			cellVolume = v[localId];
+			const double *XP = &xOverlap[3*localId];
+			const double *YP = &yOverlap[3*localId];
+			dx = XP[0]-X[0];
+			dy = XP[1]-X[1];
+			dz = XP[2]-X[2];
+			zeta = sqrt(dx*dx+dy*dy+dz*dz);
+			dx = YP[0]-Y[0];
+			dy = YP[1]-Y[1];
+			dz = YP[2]-Y[2];
+			dY = sqrt(dx*dx+dy*dy+dz*dz);
+			t = (1.0-*bondDamage)*(c1 * zeta + (1.0-*bondDamage) * OMEGA * alpha * (dY - zeta));
+			double fx = t * dx / dY;
+			double fy = t * dy / dY;
+			double fz = t * dz / dY;
+
+			*(fOwned+0) += fx*cellVolume;
+			*(fOwned+1) += fy*cellVolume;
+			*(fOwned+2) += fz*cellVolume;
+			fInternalOverlap[3*localId+0] -= fx*selfCellVolume;
+			fInternalOverlap[3*localId+1] -= fy*selfCellVolume;
+			fInternalOverlap[3*localId+2] -= fz*selfCellVolume;
+		}
+
+	}
+}
+
+void computeInternalForceIsotropicElasticPlastic
+(
+		const double* xOverlap,
+		const double* yOverlap,
+		const double* mOwned,
+		const double* volumeOverlap,
+		const double* dilatationOwned,
+		const double* bondDamage,
+		double* deviatoricPlasticExtensionState,
+		double* fInternalOverlap,
+		const int*  localNeighborList,
+		int numOwnedPoints,
+		double BULK_MODULUS,
+		double SHEAR_MODULUS,
+		double HORIZON,
+		double yieldStress
+)
+{
+
+	/*
+	 * Compute processor local contribution to internal force
+	 */
+	double K = BULK_MODULUS;
+	double MU = SHEAR_MODULUS;
+	double OMEGA=1.0;
+	double DELTA=HORIZON;
+
+	const double *xOwned = xOverlap;
+	const double *yOwned = yOverlap;
+	const double *m = mOwned;
+	const double *v = volumeOverlap;
+	const double *theta = dilatationOwned;
+	double *fOwned = fInternalOverlap;
+
+	const int *neighPtr = localNeighborList;
+	double cellVolume, alpha, dx, dy, dz, zeta, dY, t, ti, td, ed, edpN, tdTrial;
+	for(int p=0;p<numOwnedPoints;p++, xOwned +=3, yOwned +=3, fOwned+=3, m++, theta++){
+
+		int numNeigh = *neighPtr; neighPtr++;
+		const double *X = xOwned;
+		const double *Y = yOwned;
+		double weightedVol = *m;
+		alpha = 15.0*MU/weightedVol;
+		double selfCellVolume = v[p];
+		double c = 3 * K * (*theta) * OMEGA / weightedVol;
+
+		/*
+		 * Compute norm of trial stress
+		 */
+		double tdNorm = 0.0;
+		tdNorm = computeDeviatoricForceStateNorm(numNeigh,*theta,neighPtr,bondDamage,deviatoricPlasticExtensionState,X,Y,xOverlap,yOverlap,v,alpha,OMEGA);
+
+		/*
+		 * Evaluate yield function
+		 */
+		/*
+		 * 3d variety of yield value
+		 */
+		double yieldValue = 75.0 * yieldStress * yieldStress / 8 / M_PI / pow(DELTA,5);
+		/*
+		 * Planar variety of yield value
+		 */
+//		double THICKNESS=1.0;
+//		double yieldValue = 225.0 * yieldStress * yieldStress / 8 / M_PI / THICKNESS / pow(DELTA,4);
+//		double yieldValue = 0.5 * pow(15*yieldStress/weightedVol,2) * M_PI * THICKNESS * pow(DELTA,4) / 16.0;
+		double f = tdNorm * tdNorm / 2 - yieldValue;
+		bool elastic = true;
+
+//		std::cout << "Point id = " << p << std::endl;
+//		std::cout << "\tyieldStress/m^(4/5) = " << yieldStress/pow(weightedVol,4/5) << std::endl;
+//		std::cout << "\tYield Value = " << yieldValue << "; tdNorm * tdNorm / 2 = " << tdNorm * tdNorm / 2 << std::endl;
+		if(f>0){
+			/*
+			 * This step is incrementally plastic
+			 */
+			elastic = false;
+//			std::cout << "\t PLASTIC" << std::endl;
+		} else {
+//			std::cout << "\t ELASTIC" << std::endl;
+		}
+
+		for(int n=0;n<numNeigh;n++,neighPtr++,bondDamage++, deviatoricPlasticExtensionState++){
+			int localId = *neighPtr;
+			cellVolume = v[localId];
+			const double *XP = &xOverlap[3*localId];
+			const double *YP = &yOverlap[3*localId];
+			dx = XP[0]-X[0];
+			dy = XP[1]-X[1];
+			dz = XP[2]-X[2];
+			zeta = sqrt(dx*dx+dy*dy+dz*dz);
+			dx = YP[0]-Y[0];
+			dy = YP[1]-Y[1];
+			dz = YP[2]-Y[2];
+			dY = sqrt(dx*dx+dy*dy+dz*dz);
+			/*
+			 * Deviatoric extension state
+			 */
+			ed = dY-zeta-*theta*zeta/3;
+
+			/*
+			 * Deviatoric plastic extension state from last step
+			 */
+			edpN = *deviatoricPlasticExtensionState;
+
+			/*
+			 * Compute trial stress
+			 */
+			tdTrial = alpha * OMEGA * (ed - edpN);
+
+			/*
+			 * Evaluate yield function
+			 */
+			if(elastic){
+				/*
+				 * Elastic case
+				 */
+				td = tdTrial;
+
+			} else {
+				/*
+				 * Compute deviatoric force state
+				 */
+				td = sqrt(2.0*yieldValue) * tdTrial / tdNorm;
+
+				/*
+				 * Update deviatoric plastic deformation state
+				 */
+				*deviatoricPlasticExtensionState = edpN + td * ( tdNorm / sqrt(2.0*yieldValue) - 1.0 ) / alpha;
+//				std::cout << "Neighbor Id = " << localId << "; Updating deviatoricPlasticExtensionState = " << *deviatoricPlasticExtensionState << std::endl;
+			}
+//			std::cout << "\tNeighbor Id = " << localId << "\n\ttd = " << td;
+			/*
+			 * Compute isotropic part of force state
+			 */
+			ti = c * zeta;
+			/*
+			 * DEBUG
+			 */
+//			std::cout << "\n\tti = " << ti << std::endl;
+//			ti=0;
+			/*
+			 * END DEBUG
+			 */
+			/*
+			 * Force state
+			 */
+			t = ti + td;
+
+			/*
+			 * Assemble pair wise force function
+			 */
+			double fx = t * dx / dY;
+			double fy = t * dy / dY;
+			double fz = t * dz / dY;
+
+			*(fOwned+0) += fx*cellVolume;
+			*(fOwned+1) += fy*cellVolume;
+			*(fOwned+2) += fz*cellVolume;
+			fInternalOverlap[3*localId+0] -= fx*selfCellVolume;
+			fInternalOverlap[3*localId+1] -= fy*selfCellVolume;
+			fInternalOverlap[3*localId+2] -= fz*selfCellVolume;
+		}
+
+	}
+}
+
+/**
+ * Computes norm of deviatoric force state at a particular point
+ * @param numNeigh -- number of neighbors at point
+ * @param theta    -- dilatation at point
+ * @param neighPtr -- list of neighbors at point
+ * @param bondDamage     -- damage parameter for each bond at point
+ * @param X              -- original coordinates of point
+ * @param Y              -- current coordinates of point
+ * @param xOverlap       -- pointer to overlap vector of original coordinates; use this to get neighbor original coordinates
+ * @param yOverlap       -- pointer to overlap vector of current coordinates; use this to get neighbor current coordinates
+ * @param volumeOverlap  -- pointer to volume overlap vector; use this to get volume of neighboring points
+ * @param alpha          -- material property (alpha = 15 mu / m
+ * @param OMEGA          -- weight function at point
+ */
+double computeDeviatoricForceStateNorm
+(
+		int numNeigh,
+		double theta,
+		const int *neighPtr,
+		const double *bondDamage,
+		const double *deviatoricPlasticExtensionState,
+		const double *X,
+		const double *Y,
+		const double *xOverlap,
+		const double *yOverlap,
+		const double *volumeOverlap,
+		double alpha,
+		double OMEGA
+)
+{
+	double norm=0.0;
+	const double *v = volumeOverlap;
+	double cellVolume, dx, dy, dz, zeta, dY, ed, edpN, tdTrial;
+
+	for(int n=0;n<numNeigh;n++, neighPtr++, bondDamage++, deviatoricPlasticExtensionState++){
+		int localId = *neighPtr;
+		cellVolume = v[localId];
+		const double *XP = &xOverlap[3*localId];
+		const double *YP = &yOverlap[3*localId];
+		dx = XP[0]-X[0];
+		dy = XP[1]-X[1];
+		dz = XP[2]-X[2];
+		zeta = sqrt(dx*dx+dy*dy+dz*dz);
+		dx = YP[0]-Y[0];
+		dy = YP[1]-Y[1];
+		dz = YP[2]-Y[2];
+		dY = sqrt(dx*dx+dy*dy+dz*dz);
+
+		/*
+		 * Deviatoric extension state
+		 */
+		ed = dY-zeta-theta*zeta/3;
+
+		/*
+		 * Deviatoric plastic extension state from last step
+		 */
+		edpN = *deviatoricPlasticExtensionState;
+
+		/*
+		 * Compute trial stress
+		 */
+		tdTrial = alpha * OMEGA * (ed - edpN);
+
+		/*
+		 * Accumulate norm
+		 */
+		norm += tdTrial * tdTrial * cellVolume;
+
+
+//		std::cout << "computeDeviatoricForceStateNorm \n\talpha = " << alpha << "\n\tOMEGA = "<< OMEGA << std::endl;
+//		std::cout << "computeDeviatoricForceStateNorm \n\tdY = " << dY <<"\n\tzeta = " << zeta << "\n\ttheta =  "<< theta << std::endl;
+//		std::cout << "computeDeviatoricForceStateNorm \n\ted = " << dY-zeta-theta*zeta/3 << "\n\tedpN = " << edpN << std::endl;
+
+	}
+
+	return sqrt(norm);
+}
+
+}
---
+++ PdMaterialUtilities/PdMaterialUtilities.h
@@ -0,0 +1,156 @@
+// ***********************************************************************
+//
+//                             Peridigm
+//                 Copyright (2009) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+//
+// Questions?
+// David J. Littlewood   djlittl@sandia.gov
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ***********************************************************************
+
+#ifndef PDMATERIALUTILITIES_H_
+#define PDMATERIALUTILITIES_H_
+namespace PdMaterialUtilities {
+
+//void computeDilatation
+//(
+//		std::tr1::shared_ptr<double> xOverlapPtr,
+//		std::tr1::shared_ptr<double> yOverlapPtr,
+//		double *mOwned,
+//		std::tr1::shared_ptr<double> volumeOverlapPtr,
+//		double* dilatationOwned,
+//		std::tr1::shared_ptr<int> localNeighborList,
+//		int numOwnedPoints
+//);
+
+//void computeInternalForce
+//(
+//		std::tr1::shared_ptr<double> xOverlapPtr,
+//		std::tr1::shared_ptr<double> yOverlapPtr,
+//		double *mOwned,
+//		std::tr1::shared_ptr<double> volumeOverlapPtr,
+//		double *dilatationOwned,
+//		std::tr1::shared_ptr<double> fInternalOverlapPtr,
+//		std::tr1::shared_ptr<int> localNeighborList,
+//		int numOwnedPoints,
+//		Pimp::PimpIsotropicHookeSpec matSpec
+//);
+
+void computeWeightedVolume
+(
+		const double* xOverlap,
+		const double* volumeOverlap,
+		double* mOwned,
+		int myNumPoints,
+		const int* localNeighborList
+);
+
+void computeDilatation
+(
+		const double* xOverlap,
+		const double* yOverlap,
+		const double* mOwned,
+		const double* volumeOverlap,
+		const double* bondDamage,
+		double* dilatationOwned,
+		const int* localNeighborList,
+		int numOwnedPoints
+);
+
+void computeInternalForceLinearElastic
+(
+		const double* xOverlapPtr,
+		const double* yOverlapPtr,
+		const double* mOwned,
+		const double* volumeOverlapPtr,
+		const double* dilatationOwned,
+		const double* bondDamage,
+		double* fInternalOverlapPtr,
+		const int*  localNeighborList,
+		int numOwnedPoints,
+		double BULK_MODULUS,
+		double SHEAR_MODULUS
+);
+
+void computeInternalForceIsotropicElasticPlastic
+(
+		const double* xOverlap,
+		const double* yOverlap,
+		const double* mOwned,
+		const double* volumeOverlap,
+		const double* dilatationOwned,
+		const double* bondDamage,
+		double* deviatoricPlasticExtensionState,
+		double* fInternalOverlap,
+		const int*  localNeighborList,
+		int numOwnedPoints,
+		double BULK_MODULUS,
+		double SHEAR_MODULUS,
+		double HORIZON,
+		double yieldStress
+);
+
+void updateGeometry
+(
+		const double* xOverlap,
+		const double* uOverlap,
+		const double* velocityOverlap,
+		double* yOverlap,
+		int overLapLength,
+		double dt
+);
+
+/**
+ * Computes norm of deviatoric force state at a particular point
+ * @param numNeigh -- number of neighbors at point
+ * @param theta    -- dilatation at point
+ * @param neighPtr -- list of neighbors at point
+ * @param bondDamage     -- damage parameter for each bond at point
+ * @param X              -- original coordinates of point
+ * @param Y              -- current coordinates of point
+ * @param xOverlap       -- pointer to overlap vector of original coordinates; use this to get neighbor original coordinates
+ * @param yOverlap       -- pointer to overlap vector of current coordinates; use this to get neighbor current coordinates
+ * @param volumeOverlap  -- pointer to volume overlap vector; use this to get volume of neighboring points
+ * @param alpha          -- material property (alpha = 15 mu / m
+ * @param OMEGA          -- weight function at point
+ */
+double computeDeviatoricForceStateNorm
+(
+		int numNeigh,
+		double theta,
+		const int *neighPtr,
+		const double *bondDamage,
+		const double *deviatoricPlasticExtensionState,
+		const double *X,
+		const double *Y,
+		const double *xOverlap,
+		const double *yOverlap,
+		const double *volumeOverlap,
+		double alpha,
+		double OMEGA
+);
+
+}
+
+#endif /* PDMATERIALUTILITIES_H_ */
