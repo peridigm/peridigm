5c46022bac38a7e6dcd83baa93741c5f790d07ce
David Littlewood <djlittl@sandia.gov>
2010-09-02 23:05:01 +0000

    Fleshed out parallel unit test for PdQuickGridDiscretization.
    Now checks the same things as the serial version.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@415 d9297710-12f9-482b-b11f-fb677dc88bbb

--- src/utPeridigm_PdQuickGridDiscretization_MPI_np2.cpp
+++ src/utPeridigm_PdQuickGridDiscretization_MPI_np2.cpp
@@ -44,6 +44,10 @@ using namespace PeridigmNS;
 
 void simpleTensorProductMesh()
 {
+  int rank, numProcs;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+
   Teuchos::RCP<Epetra_Comm> comm;
   comm = rcp(new Epetra_MpiComm(MPI_COMM_WORLD));
   RCP<ParameterList> discParams = rcp(new ParameterList);
@@ -69,158 +73,243 @@ void simpleTensorProductMesh()
   RCP<PdQuickGridDiscretization> discretization =
     rcp(new PdQuickGridDiscretization(comm, discParams));
 
-//   // sanity check, calling with a dimension other than 1 or 3 should throw an exception
-//   BOOST_CHECK_THROW(discretization->getMap(0), Teuchos::Exceptions::InvalidParameter);
-//   BOOST_CHECK_THROW(discretization->getMap(2), Teuchos::Exceptions::InvalidParameter);
-//   BOOST_CHECK_THROW(discretization->getMap(4), Teuchos::Exceptions::InvalidParameter);
+  // sanity check, calling with a dimension other than 1 or 3 should throw an exception
+  BOOST_CHECK_THROW(discretization->getMap(0), Teuchos::Exceptions::InvalidParameter);
+  BOOST_CHECK_THROW(discretization->getMap(2), Teuchos::Exceptions::InvalidParameter);
+  BOOST_CHECK_THROW(discretization->getMap(4), Teuchos::Exceptions::InvalidParameter);
 
-//   // basic checks on the 1d map
-//   Teuchos::RCP<const Epetra_BlockMap> map = discretization->getMap(1);
-//   BOOST_CHECK(map->NumGlobalElements() == 8);
-//   BOOST_CHECK(map->NumMyElements() == 8);
-//   BOOST_CHECK(map->ElementSize() == 1);
-//   BOOST_CHECK(map->IndexBase() == 0);
-//   BOOST_CHECK(map->UniqueGIDs() == true);
-//   int* myGlobalElements = map->MyGlobalElements();
-//   for(int i=0 ; i<map->NumMyElements() ; ++i)
-//     BOOST_CHECK(myGlobalElements[i] == i);
+  // basic checks on the 1d map
+  Teuchos::RCP<const Epetra_BlockMap> map = discretization->getMap(1);
+  BOOST_CHECK(map->NumGlobalElements() == 8);
+  BOOST_CHECK(map->NumMyElements() == 4);
+  BOOST_CHECK(map->ElementSize() == 1);
+  BOOST_CHECK(map->IndexBase() == 0);
+  BOOST_CHECK(map->UniqueGIDs() == true);
+  int* myGlobalElements = map->MyGlobalElements();
+  if(rank == 0){
+    BOOST_CHECK(myGlobalElements[0] == 0);
+    BOOST_CHECK(myGlobalElements[1] == 2);
+    BOOST_CHECK(myGlobalElements[2] == 4);
+    BOOST_CHECK(myGlobalElements[3] == 6);
+  }
+  if(rank == 1){
+    BOOST_CHECK(myGlobalElements[0] == 5);
+    BOOST_CHECK(myGlobalElements[1] == 7);
+    BOOST_CHECK(myGlobalElements[2] == 1);
+    BOOST_CHECK(myGlobalElements[3] == 3);
+  }
 
-//   // for the serial case, the map and the overlap map should match
-//   Teuchos::RCP<const Epetra_BlockMap> overlapMap = discretization->getOverlapMap(1);
-//   BOOST_CHECK(map->SameAs(*overlapMap) == true);
+  // check the 1d overlap map
+  // for this simple discretization, everything should be ghosted on both processors
+  Teuchos::RCP<const Epetra_BlockMap> overlapMap = discretization->getOverlapMap(1);
+  BOOST_CHECK(overlapMap->NumGlobalElements() == 16);
+  BOOST_CHECK(overlapMap->NumMyElements() == 8);
+  BOOST_CHECK(overlapMap->ElementSize() == 1);
+  BOOST_CHECK(overlapMap->IndexBase() == 0);
+  BOOST_CHECK(overlapMap->UniqueGIDs() == false);
+  myGlobalElements = overlapMap->MyGlobalElements();
+  if(rank == 0){
+    BOOST_CHECK(myGlobalElements[0] == 0);
+    BOOST_CHECK(myGlobalElements[1] == 2);
+    BOOST_CHECK(myGlobalElements[2] == 4);
+    BOOST_CHECK(myGlobalElements[3] == 6);
+    BOOST_CHECK(myGlobalElements[4] == 1);
+    BOOST_CHECK(myGlobalElements[5] == 3);
+    BOOST_CHECK(myGlobalElements[6] == 5);
+    BOOST_CHECK(myGlobalElements[7] == 7);
+  }
+  if(rank == 1){
+    BOOST_CHECK(myGlobalElements[0] == 5);
+    BOOST_CHECK(myGlobalElements[1] == 7);
+    BOOST_CHECK(myGlobalElements[2] == 1);
+    BOOST_CHECK(myGlobalElements[3] == 3);
+    BOOST_CHECK(myGlobalElements[4] == 0);
+    BOOST_CHECK(myGlobalElements[5] == 2);
+    BOOST_CHECK(myGlobalElements[6] == 4);
+    BOOST_CHECK(myGlobalElements[7] == 6);
+  }
 
-//   // same checks for 3d map
-//   map = discretization->getMap(3);
-//   BOOST_CHECK(map->NumGlobalElements() == 8);
-//   BOOST_CHECK(map->NumMyElements() == 8);
-//   BOOST_CHECK(map->ElementSize() == 3);
-//   BOOST_CHECK(map->IndexBase() == 0);
-//   BOOST_CHECK(map->UniqueGIDs() == true);
-//   myGlobalElements = map->MyGlobalElements();
-//   for(int i=0 ; i<map->NumMyElements() ; ++i)
-//     BOOST_CHECK(myGlobalElements[i] == i);
+  // same checks for 3d map
+  map = discretization->getMap(3);
+  BOOST_CHECK(map->NumGlobalElements() == 8);
+  BOOST_CHECK(map->NumMyElements() == 4);
+  BOOST_CHECK(map->ElementSize() == 3);
+  BOOST_CHECK(map->IndexBase() == 0);
+  BOOST_CHECK(map->UniqueGIDs() == true);
+  myGlobalElements = map->MyGlobalElements();
+  if(rank == 0){
+    BOOST_CHECK(myGlobalElements[0] == 0);
+    BOOST_CHECK(myGlobalElements[1] == 2);
+    BOOST_CHECK(myGlobalElements[2] == 4);
+    BOOST_CHECK(myGlobalElements[3] == 6);
+  }
+  if(rank == 1){
+    BOOST_CHECK(myGlobalElements[0] == 5);
+    BOOST_CHECK(myGlobalElements[1] == 7);
+    BOOST_CHECK(myGlobalElements[2] == 1);
+    BOOST_CHECK(myGlobalElements[3] == 3);
+  }
 
-//   // for the serial case, the map and the overlap map should match
-//   overlapMap = discretization->getOverlapMap(3);
-//   BOOST_CHECK(map->SameAs(*overlapMap) == true);
+  // check the 3d overlap map
+  // for this simple discretization, everything should be ghosted on both processors
+  overlapMap = discretization->getOverlapMap(3);
+  BOOST_CHECK(overlapMap->NumGlobalElements() == 16);
+  BOOST_CHECK(overlapMap->NumMyElements() == 8);
+  BOOST_CHECK(overlapMap->ElementSize() == 3);
+  BOOST_CHECK(overlapMap->IndexBase() == 0);
+  BOOST_CHECK(overlapMap->UniqueGIDs() == false);
+  myGlobalElements = overlapMap->MyGlobalElements();
+  if(rank == 0){
+    BOOST_CHECK(myGlobalElements[0] == 0);
+    BOOST_CHECK(myGlobalElements[1] == 2);
+    BOOST_CHECK(myGlobalElements[2] == 4);
+    BOOST_CHECK(myGlobalElements[3] == 6);
+    BOOST_CHECK(myGlobalElements[4] == 1);
+    BOOST_CHECK(myGlobalElements[5] == 3);
+    BOOST_CHECK(myGlobalElements[6] == 5);
+    BOOST_CHECK(myGlobalElements[7] == 7);
+  }
+  if(rank == 1){
+    BOOST_CHECK(myGlobalElements[0] == 5);
+    BOOST_CHECK(myGlobalElements[1] == 7);
+    BOOST_CHECK(myGlobalElements[2] == 1);
+    BOOST_CHECK(myGlobalElements[3] == 3);
+    BOOST_CHECK(myGlobalElements[4] == 0);
+    BOOST_CHECK(myGlobalElements[5] == 2);
+    BOOST_CHECK(myGlobalElements[6] == 4);
+    BOOST_CHECK(myGlobalElements[7] == 6);
+  }
 
-//   // check the bond map
-//   // the horizon was chosen such that each point should have three neighbors
-//   // note that if the NeighborhoodType parameter is not set to Spherical, this will fail
-//   Teuchos::RCP<const Epetra_BlockMap> bondMap = discretization->getBondMap();
-//   BOOST_CHECK(bondMap->NumGlobalElements() == 8*3);
-//   BOOST_CHECK(bondMap->NumMyElements() == 8*3);
-//   BOOST_CHECK(bondMap->ElementSize() == 1);
-//   BOOST_CHECK(bondMap->IndexBase() == 0);
-//   BOOST_CHECK(bondMap->UniqueGIDs() == true);
-//   myGlobalElements = bondMap->MyGlobalElements();
-//   for(int i=0 ; i<bondMap->NumMyElements() ; ++i)
-//     BOOST_CHECK(myGlobalElements[i] == i); 
+  // check the bond map
+  // the horizon was chosen such that each point should have three neighbors
+  // note that if the NeighborhoodType parameter is not set to Spherical, this will fail
+  Teuchos::RCP<const Epetra_BlockMap> bondMap = discretization->getBondMap();
+  BOOST_CHECK(bondMap->NumGlobalElements() == 8*3);
+  BOOST_CHECK(bondMap->NumMyElements() == 4*3);
+  BOOST_CHECK(bondMap->ElementSize() == 1);
+  BOOST_CHECK(bondMap->IndexBase() == 0);
+  BOOST_CHECK(bondMap->UniqueGIDs() == true);
+  myGlobalElements = bondMap->MyGlobalElements();
+  if(rank == 0){
+    for(int i=0 ; i<bondMap->NumMyElements() ; ++i)
+      BOOST_CHECK(myGlobalElements[i] == i); 
+  }
+  if(rank == 1){
+    for(int i=0 ; i<bondMap->NumMyElements() ; ++i)
+      BOOST_CHECK(myGlobalElements[i] == i+12); 
+  }
+  BOOST_CHECK(discretization->getNumBonds() == 4*3);
 
-//   BOOST_CHECK(discretization->getNumBonds() == 8*3);
+  // check the initial positions
+  // all three coordinates are contained in a single vector
+  Teuchos::RCP<Epetra_Vector> initialX = discretization->getInitialX();
+  BOOST_CHECK(initialX->MyLength() == 4*3);
+  BOOST_CHECK(initialX->GlobalLength() == 8*3);
+  if(rank == 0){
+    BOOST_CHECK_CLOSE((*initialX)[0],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[1],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[2],  0.25, 1.0e-16);
+ 
+    BOOST_CHECK_CLOSE((*initialX)[3],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[4],  0.75, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[5],  0.25, 1.0e-16);
 
-//   // check the initial positions
-//   // all three coordinates are contained in a single vector
-//   Teuchos::RCP<Epetra_Vector> initialX = discretization->getInitialX();
-//   BOOST_CHECK(initialX->MyLength() == 8*3);
-//   BOOST_CHECK(initialX->GlobalLength() == 8*3);
+    BOOST_CHECK_CLOSE((*initialX)[6],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[7],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[8],  0.75, 1.0e-16);
 
-//   BOOST_CHECK_CLOSE((*initialX)[0],  0.25, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[1],  0.25, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[2],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[9],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[10], 0.75, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[11], 0.75, 1.0e-16);
+  }
+  if(rank == 1){
+    BOOST_CHECK_CLOSE((*initialX)[0],  0.75, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[1],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[2],  0.75, 1.0e-16);
 
-//   BOOST_CHECK_CLOSE((*initialX)[3],  0.75, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[4],  0.25, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[5],  0.25, 1.0e-16);
-  
-//   BOOST_CHECK_CLOSE((*initialX)[6],  0.25, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[7],  0.75, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[8],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[3],  0.75, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[4],  0.75, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[5],  0.75, 1.0e-16);
 
-//   BOOST_CHECK_CLOSE((*initialX)[9],  0.75, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[10], 0.75, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[11], 0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[6],  0.75, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[7],  0.25, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[8],  0.25, 1.0e-16);
 
-//   BOOST_CHECK_CLOSE((*initialX)[12], 0.25, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[13], 0.25, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[14], 0.75, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[9],  0.75, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[10], 0.75, 1.0e-16);
+    BOOST_CHECK_CLOSE((*initialX)[11], 0.25, 1.0e-16);
+  }
 
-//   BOOST_CHECK_CLOSE((*initialX)[15], 0.75, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[16], 0.25, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[17], 0.75, 1.0e-16);
+  // check cell volumes
+  Teuchos::RCP<Epetra_Vector> volume = discretization->getCellVolume();
+  BOOST_CHECK(volume->MyLength() == 4);
+  BOOST_CHECK(volume->GlobalLength() == 8);
+  for(int i=0 ; i<volume->MyLength() ; ++i)
+    BOOST_CHECK_CLOSE((*volume)[i], 0.125, 1.0e-16);
 
-//   BOOST_CHECK_CLOSE((*initialX)[18], 0.25, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[19], 0.75, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[20], 0.75, 1.0e-16);
+  // check the neighbor lists
+  Teuchos::RCP<PeridigmNS::NeighborhoodData> neighborhoodData = discretization->getNeighborhoodData();
+  BOOST_CHECK(neighborhoodData->NumOwnedPoints() == 4);
+  int* ownedIds = neighborhoodData->OwnedIDs();
+  BOOST_CHECK(ownedIds[0] == 0);
+  BOOST_CHECK(ownedIds[1] == 1);
+  BOOST_CHECK(ownedIds[2] == 2);
+  BOOST_CHECK(ownedIds[3] == 3);
+  BOOST_CHECK(neighborhoodData->NeighborhoodListSize() == 16);
+  int* neighborhood = neighborhoodData->NeighborhoodList();
+  int* neighborhoodPtr = neighborhoodData->NeighborhoodPtr();
+  // remember, these are local IDs on each processor, 
+  // which includes both owned and ghost nodes (confusing!)
+  if(rand == 0){
+    BOOST_CHECK(neighborhoodPtr[0] == 0);
+    BOOST_CHECK(neighborhood[0]    == 3);
+    BOOST_CHECK(neighborhood[1]    == 1);
+    BOOST_CHECK(neighborhood[2]    == 2);
+    BOOST_CHECK(neighborhood[3]    == 4);
 
-//   BOOST_CHECK_CLOSE((*initialX)[21], 0.75, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[22], 0.75, 1.0e-16);
-//   BOOST_CHECK_CLOSE((*initialX)[23], 0.75, 1.0e-16);
+    BOOST_CHECK(neighborhoodPtr[1] == 4);
+    BOOST_CHECK(neighborhood[4]    == 3);
+    BOOST_CHECK(neighborhood[5]    == 0);
+    BOOST_CHECK(neighborhood[6]    == 3);
+    BOOST_CHECK(neighborhood[7]    == 5);
 
-//   // check cell volumes
-//   Teuchos::RCP<Epetra_Vector> volume = discretization->getCellVolume();
-//   BOOST_CHECK(volume->MyLength() == 8);
-//   BOOST_CHECK(volume->GlobalLength() == 8);
-//   for(int i=0 ; i<volume->MyLength() ; ++i)
-//     BOOST_CHECK_CLOSE((*volume)[i], 0.125, 1.0e-16);
+    BOOST_CHECK(neighborhoodPtr[2] == 8);
+    BOOST_CHECK(neighborhood[8]    == 3);
+    BOOST_CHECK(neighborhood[9]    == 0);
+    BOOST_CHECK(neighborhood[10]   == 3);
+    BOOST_CHECK(neighborhood[11]   == 6);
 
-//   // check the neighbor lists
-//   Teuchos::RCP<PeridigmNS::NeighborhoodData> neighborhoodData = discretization->getNeighborhoodData();
-//   BOOST_CHECK(neighborhoodData->NumOwnedPoints() == 8);
-//   int* ownedIds = neighborhoodData->OwnedIDs();
-//   for(int i=0 ; i<neighborhoodData->NumOwnedPoints() ; ++i)
-//     BOOST_CHECK(ownedIds[i] == i);
-//   BOOST_CHECK(neighborhoodData->NeighborhoodListSize() == 32);
-//   int* neighborhood = neighborhoodData->NeighborhoodList();
-//   int* neighborhoodPtr = neighborhoodData->NeighborhoodPtr();
+    BOOST_CHECK(neighborhoodPtr[3] == 12);
+    BOOST_CHECK(neighborhood[12]   == 3);
+    BOOST_CHECK(neighborhood[13]   == 1);
+    BOOST_CHECK(neighborhood[14]   == 2);
+    BOOST_CHECK(neighborhood[15]   == 7);
+  }
+  if(rank == 1){
+    BOOST_CHECK(neighborhoodPtr[0] == 0);
+    BOOST_CHECK(neighborhood[0]    == 3);
+    BOOST_CHECK(neighborhood[1]    == 2);
+    BOOST_CHECK(neighborhood[2]    == 6);
+    BOOST_CHECK(neighborhood[3]    == 1);
 
-//   BOOST_CHECK(neighborhoodPtr[0] == 0);
-//   BOOST_CHECK(neighborhood[0]    == 3);
-//   BOOST_CHECK(neighborhood[1]    == 1);
-//   BOOST_CHECK(neighborhood[2]    == 2);
-//   BOOST_CHECK(neighborhood[3]    == 4);
+    BOOST_CHECK(neighborhoodPtr[1] == 4);
+    BOOST_CHECK(neighborhood[4]    == 3);
+    BOOST_CHECK(neighborhood[5]    == 3);
+    BOOST_CHECK(neighborhood[6]    == 0);
+    BOOST_CHECK(neighborhood[7]    == 7);
 
-//   BOOST_CHECK(neighborhoodPtr[1] == 4);
-//   BOOST_CHECK(neighborhood[4]    == 3);
-//   BOOST_CHECK(neighborhood[5]    == 0);
-//   BOOST_CHECK(neighborhood[6]    == 3);
-//   BOOST_CHECK(neighborhood[7]    == 5);
+    BOOST_CHECK(neighborhoodPtr[2] == 8);
+    BOOST_CHECK(neighborhood[8]    == 3);
+    BOOST_CHECK(neighborhood[9]    == 4);
+    BOOST_CHECK(neighborhood[10]   == 3);
+    BOOST_CHECK(neighborhood[11]   == 0);
 
-//   BOOST_CHECK(neighborhoodPtr[2] == 8);
-//   BOOST_CHECK(neighborhood[8]    == 3);
-//   BOOST_CHECK(neighborhood[9]    == 0);
-//   BOOST_CHECK(neighborhood[10]   == 3);
-//   BOOST_CHECK(neighborhood[11]   == 6);
-
-//   BOOST_CHECK(neighborhoodPtr[3] == 12);
-//   BOOST_CHECK(neighborhood[12]   == 3);
-//   BOOST_CHECK(neighborhood[13]   == 1);
-//   BOOST_CHECK(neighborhood[14]   == 2);
-//   BOOST_CHECK(neighborhood[15]   == 7);
-
-//   BOOST_CHECK(neighborhoodPtr[4] == 16);
-//   BOOST_CHECK(neighborhood[16]   == 3);
-//   BOOST_CHECK(neighborhood[17]   == 0);
-//   BOOST_CHECK(neighborhood[18]   == 5);
-//   BOOST_CHECK(neighborhood[19]   == 6);
-
-//   BOOST_CHECK(neighborhoodPtr[5] == 20);
-//   BOOST_CHECK(neighborhood[20]   == 3);
-//   BOOST_CHECK(neighborhood[21]   == 1);
-//   BOOST_CHECK(neighborhood[22]   == 4);
-//   BOOST_CHECK(neighborhood[23]   == 7);
-
-//   BOOST_CHECK(neighborhoodPtr[6] == 24);
-//   BOOST_CHECK(neighborhood[24]   == 3);
-//   BOOST_CHECK(neighborhood[25]   == 2);
-//   BOOST_CHECK(neighborhood[26]   == 4);
-//   BOOST_CHECK(neighborhood[27]   == 7);
-
-//   BOOST_CHECK(neighborhoodPtr[7] == 28);
-//   BOOST_CHECK(neighborhood[28]   == 3);
-//   BOOST_CHECK(neighborhood[29]   == 3);
-//   BOOST_CHECK(neighborhood[30]   == 5);
-//   BOOST_CHECK(neighborhood[31]   == 6);
+    BOOST_CHECK(neighborhoodPtr[3] == 12);
+    BOOST_CHECK(neighborhood[12]   == 3);
+    BOOST_CHECK(neighborhood[13]   == 2);
+    BOOST_CHECK(neighborhood[14]   == 5);
+    BOOST_CHECK(neighborhood[15]   == 1);
+  }
 }
 
 bool init_unit_test_suite()
