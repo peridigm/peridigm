4ec42654b52ad886f70d623f600cd9fdcd2717f5
David Littlewood <djlittl@sandia.gov>
2010-09-09 17:33:51 +0000

    Created unit test for Peridigm object.
    Currently a no-op, this is just infrastructure.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@418 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 4479dfdaaef4371e46b26b9fae5bde44dbb03fe9

---
+++ src/utPeridigm.cpp
@@ -0,0 +1,103 @@
+/*! \file utPeridigm.cpp */
+
+// ***********************************************************************
+//
+//                             Peridigm
+//                 Copyright (2009) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// 
+// Questions? 
+// David J. Littlewood   djlittl@sandia.gov 
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ***********************************************************************
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_ALTERNATIVE_INIT_API
+#include <boost/test/unit_test.hpp>
+#include <Epetra_ConfigDefs.h> // used to define HAVE_MPI
+#ifdef HAVE_MPI
+  #include <Epetra_MpiComm.h>
+#else
+  #include <Epetra_SerialComm.h>
+#endif
+#include "Peridigm.hpp"
+
+using namespace boost::unit_test;
+using namespace Teuchos;
+using namespace PeridigmNS;
+
+void initialize()
+{
+  Teuchos::RCP<Epetra_Comm> comm;
+  #ifdef HAVE_MPI
+    comm = rcp(new Epetra_MpiComm(MPI_COMM_WORLD));
+  #else
+    comm = rcp(new Epetra_SerialComm);
+  #endif
+
+    BOOST_CHECK(1 == 1);  
+
+    //RCP<ParameterList> discParams = rcp(new ParameterList);
+
+//   discParams->set("Horizon", 0.501);
+//   ParameterList& quickGridParams = discParams->sublist("TensorProduct3DMeshGenerator");
+
+    //  BOOST_CHECK_THROW(discretization->getMap(0), Teuchos::Exceptions::InvalidParameter);
+
+  // BOOST_CHECK(map->NumGlobalElements() == 8);
+  //BOOST_CHECK_CLOSE((*initialX)[0],  0.25, 1.0e-16);
+
+}
+
+bool init_unit_test_suite()
+{
+	// Add a suite for each processor in the test
+	bool success = true;
+
+	test_suite* proc = BOOST_TEST_SUITE("utPeridigm");
+	proc->add(BOOST_TEST_CASE(&initialize));
+	framework::master_test_suite().add(proc);
+
+	return success;
+}
+
+bool init_unit_test()
+{
+	init_unit_test_suite();
+	return true;
+}
+
+int main
+(int argc, char* argv[])
+{
+  #ifdef HAVE_MPI
+    MPI_Init(&argc,&argv);
+  #endif
+
+  // Initialize UTF
+  return unit_test_main(init_unit_test, argc, argv);
+
+  #ifdef HAVE_MPI
+    MPI_Finalize();
+  #endif
+}
