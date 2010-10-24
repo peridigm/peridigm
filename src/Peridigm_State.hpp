5ffd71fb9a70f3fe1cda54efbb4173eb30cc5a26
David Littlewood <djlittl@sandia.gov>
2010-10-24 05:51:01 +0000

    Continued work on data handling.
    Created a State class and a DataManager class.
    
    Very much still in the planning stage here,
    neither is even hooked up to the build system.
    
    The idea is that the DataManager is the magic object
    that gets passed to the material models and I/O system.
    There will be an interface that provides access to the
    underlying data for a given FieldSpec and step (N,
    NP1, or NONE).
    
    Inside the data manager, data will be organized within
    State objects.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@486 d9297710-12f9-482b-b11f-fb677dc88bbb

---
+++ src/Peridigm_State.hpp
@@ -0,0 +1,73 @@
+/*! \file Peridigm_State.hpp */
+
+// ***********************************************************************
+//
+//                             Peridigm
+//                 Copyright (2009) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// 
+// Questions? 
+// David J. Littlewood   djlittl@sandia.gov 
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ***********************************************************************
+
+#ifndef PERIDIGM_STATE_HPP
+#define PERIDIGM_STATE_HPP
+
+#include <Epetra_BlockMap>
+
+namespace PeridigmNS {
+
+class State {
+
+public:
+
+  State(){}
+  State(const State& state){}
+  ~State(){}
+
+  void allocateScalarData(int numVar, Teuchos::RCP<Epetra_BlockMap> map)
+  {
+    scalarData = rcp(new Epetra_MultiVector(map, numVar));
+  }
+
+  void allocateVector1DData(int numVar, Teuchos::RCP<Epetra_BlockMap> map)
+  {
+    vector2DData = rcp(new Epetra_MultiVector(map, numVar));
+  }
+
+  void allocateVector2DData(int numVar, Teuchos::RCP<Epetra_BlockMap> map)
+  {
+    vector3DData = rcp(new Epetra_MultiVector(map, numVar));
+  }
+
+protected:
+
+  Teuchos::RCP<Epetra_MultiVector> scalarData;
+  Teuchos::RCP<Epetra_MultiVector> vector2DData;
+  Teuchos::RCP<Epetra_MultiVector> vector3DData;
+};
+
+}
+
+#endif // PERIDIGM_STATE_HPP
