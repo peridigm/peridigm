ae49376f181766f9a3be6bf73f6cee50f3eafab9
John Mitchell <jamitch@sandia.gov>
2010-10-28 22:14:49 +0000

    Now the plasticity unit test diffs computed results.  Adds 'ep.gold.dat'
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@495 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 1638faf65295ba1a6584d3851dd0fa8348430e33

--- src/materials/unitTest/utPeridigm_IsotropicElasticPlastic/run_unit_test.py
+++ src/materials/unitTest/utPeridigm_IsotropicElasticPlastic/run_unit_test.py
@@ -7,19 +7,27 @@ from subprocess import Popen
 
 if __name__ == "__main__":
 
-    executable = sys.argv[-1]
-    base_name = string.splitfields(executable, '/')[-1]
-    logfile = open(base_name + ".log", 'w')
+	executable = sys.argv[-1]
+	base_name = string.splitfields(executable, '/')[-1]
+	logfile = open(base_name + ".log", 'w')
 
-    if not os.path.exists(executable):
-        logfile.write("\nError:  " + executable + " not found!\n\n")
-        sys.exit(1)
+	if not os.path.exists(executable):
+		logfile.write("\nError:  " + executable + " not found!\n\n")
+		sys.exit(1)
 
-    command = []
-    for i in range(len(sys.argv)-1):
-        command.append(sys.argv[1+i])
+	command = []
+	for i in range(len(sys.argv)-1):
+		command.append(sys.argv[1+i])
 
-    p = Popen(command, stdout=logfile, stderr=logfile)
-    return_code = p.wait()
+	p = Popen(command, stdout=logfile, stderr=logfile)
+	return_code = p.wait()
+	if 0 != return_code:
+		sys.exit(return_code)
 
-    sys.exit(return_code)
+
+	# OK -- successful to here
+	#  Now diff files
+	c="diff ep.dat ep.gold.dat"
+	p = Popen(['/bin/bash','-c',c], stdout=logfile, stderr=logfile)
+	return_code = p.wait()
+	sys.exit(return_code)
