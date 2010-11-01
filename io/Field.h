774b1307b5b2a70f8ce45aaa434cb3d8d625ff14
David Littlewood <djlittl@sandia.gov>
2010-11-01 18:09:23 +0000

    Added stateArch member variable to FieldSpec.
    Its type is:
    
    enum FieldStateArchitecture {STATELESS=0, STATEFUL=1};
    
    I updated the FieldSpec constructors.  There is a new one
    that takes a FieldStateArchitecture argument.  The old one
    is still there and assigned STATELESS to the stateArch.
    
    Updated all the hard-coded FieldSpecs to include a stateArch.
    
    Also updated the equality and inequality operators for FieldSpec.
    
    Note to John, I did not update FieldSpec::operator< because
    I wasn't sure how you'd want it.  So, it currently does nothing
    with stateArch.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@499 d9297710-12f9-482b-b11f-fb677dc88bbb

--- io/Field.h
+++ io/Field.h
@@ -24,43 +24,47 @@ struct FieldSpec {
 public:
 	enum FieldType {VOLUME=0, ID, PROC_NUM, WEIGHTED_VOLUME, DILATATION, DAMAGE, E_DP, NUM_NEIGHBORS, COORDINATES, DISPLACEMENT, VELOCITY, ACCELERATION, FORCE, FORCE_DENSITY, DEFAULT_FIELDTYPE};
 	enum FieldLength {SCALAR=1, VECTOR2D=2, VECTOR3D=3};
+    enum FieldStateArchitecture {STATELESS=0, STATEFUL=1}; 
     enum FieldStep {STEP_N=0, STEP_NP1=1, STEP_NONE=2};
 
 private:
 	FieldType type;
 	FieldLength length;
+    FieldStateArchitecture stateArch;
 	std::string label;
 
 public:
 	FieldSpec(const FieldSpec& c);
-	explicit FieldSpec(FieldType t, FieldLength len, const string& label);
+    explicit FieldSpec(FieldType t, FieldLength len, const string& label);
+    explicit FieldSpec(FieldType t, FieldLength len, FieldStateArchitecture arch, const string& label);
 	bool operator == (const FieldSpec& right) const;
 	bool operator != (const FieldSpec& right) const;
 	bool operator < (const FieldSpec& right) const;
 	FieldType getType() const;
 	FieldLength getLength() const;
+    FieldStateArchitecture getStateArchitecture() const;
 
 	string getLabel() const;
 };
 
 // Scalar FieldSpecs
-const FieldSpec DEFAULT_FIELDTYPE(FieldSpec::DEFAULT_FIELDTYPE,FieldSpec::SCALAR, "Default_FieldType");
-const FieldSpec VOLUME(FieldSpec::VOLUME,FieldSpec::SCALAR, "Volume");
-const FieldSpec ID(Field_NS::FieldSpec::ID,Field_NS::FieldSpec::SCALAR, "Id");
-const FieldSpec PROC_NUM(Field_NS::FieldSpec::PROC_NUM,Field_NS::FieldSpec::SCALAR, "Proc_Num");
-const FieldSpec DAMAGE(FieldSpec::VOLUME,FieldSpec::SCALAR, "Damage");
-const FieldSpec WEIGHTED_VOLUME(FieldSpec::WEIGHTED_VOLUME,FieldSpec::SCALAR, "Weighted_Volume");
-const FieldSpec DILATATION(FieldSpec::DILATATION,FieldSpec::SCALAR, "Dilatation");
-const FieldSpec NUM_NEIGHBORS(FieldSpec::NUM_NEIGHBORS,FieldSpec::SCALAR, "Num_Neighbors");
-const FieldSpec DEVIATORIC_PLASTIC_EXTENSION(FieldSpec::E_DP,FieldSpec::SCALAR, "Deviatoric_Plastic_Extension");
+const FieldSpec DEFAULT_FIELDTYPE(FieldSpec::DEFAULT_FIELDTYPE, FieldSpec::SCALAR, FieldSpec::STATELESS, "Default_FieldType");
+const FieldSpec VOLUME(FieldSpec::VOLUME,                       FieldSpec::SCALAR, FieldSpec::STATELESS, "Volume");
+const FieldSpec ID(Field_NS::FieldSpec::ID,                     FieldSpec::SCALAR, FieldSpec::STATELESS, "Id");
+const FieldSpec PROC_NUM(Field_NS::FieldSpec::PROC_NUM,         FieldSpec::SCALAR, FieldSpec::STATELESS, "Proc_Num");
+const FieldSpec DAMAGE(FieldSpec::DAMAGE,                       FieldSpec::SCALAR, FieldSpec::STATEFUL,  "Damage");
+const FieldSpec WEIGHTED_VOLUME(FieldSpec::WEIGHTED_VOLUME,     FieldSpec::SCALAR, FieldSpec::STATEFUL,  "Weighted_Volume");
+const FieldSpec DILATATION(FieldSpec::DILATATION,               FieldSpec::SCALAR, FieldSpec::STATEFUL,  "Dilatation");
+const FieldSpec NUM_NEIGHBORS(FieldSpec::NUM_NEIGHBORS,         FieldSpec::SCALAR, FieldSpec::STATELESS, "Num_Neighbors");
+const FieldSpec DEVIATORIC_PLASTIC_EXTENSION(FieldSpec::E_DP,   FieldSpec::SCALAR, FieldSpec::STATEFUL,  "Deviatoric_Plastic_Extension");
 
 // Vector FieldSpecs
-const FieldSpec COORD3D(FieldSpec::COORDINATES,FieldSpec::VECTOR3D, "Coordinates");
-const FieldSpec DISPL3D(FieldSpec::DISPLACEMENT,FieldSpec::VECTOR3D, "Displacement");
-const FieldSpec VELOC3D(FieldSpec::VELOCITY,    FieldSpec::VECTOR3D, "Velocity");
-const FieldSpec ACCEL3D(FieldSpec::ACCELERATION,FieldSpec::VECTOR3D, "Acceleration");
-const FieldSpec FORCE3D(FieldSpec::FORCE,FieldSpec::VECTOR3D, "Force");
-const FieldSpec FORCE_DENSITY3D(FieldSpec::FORCE_DENSITY,FieldSpec::VECTOR3D, "Force Density");
+const FieldSpec COORD3D(FieldSpec::COORDINATES,           FieldSpec::VECTOR3D, FieldSpec::STATELESS, "Coordinates");
+const FieldSpec DISPL3D(FieldSpec::DISPLACEMENT,          FieldSpec::VECTOR3D, FieldSpec::STATEFUL,  "Displacement");
+const FieldSpec VELOC3D(FieldSpec::VELOCITY,              FieldSpec::VECTOR3D, FieldSpec::STATEFUL,  "Velocity");
+const FieldSpec ACCEL3D(FieldSpec::ACCELERATION,          FieldSpec::VECTOR3D, FieldSpec::STATEFUL,  "Acceleration");
+const FieldSpec FORCE3D(FieldSpec::FORCE,                 FieldSpec::VECTOR3D, FieldSpec::STATEFUL,  "Force");
+const FieldSpec FORCE_DENSITY3D(FieldSpec::FORCE_DENSITY, FieldSpec::VECTOR3D, FieldSpec::STATEFUL,  "Force Density");
 
 template<typename T>
 class Field : public FieldSpec {
