5f1a7eb856e12cb73c41ab7ea6e8c756563d1198
David Littlewood <djlittl@sandia.gov>
2010-11-01 18:09:23 +0000

    Added stateArch member variable to FieldSpec.
    Its type is:
    
    enum FieldStateArchitecture {STATELESS=0, STATEFUL=1};
    
    I updated the FieldSpec constructors.  There is a new one
    that takes a FieldStateArchitecture argument.  The old one
    is still there and assigned STATELESS to the stateArch.
    
    Updated all the hard-coded FieldSpecs to include a stateArch.
    
    Also updated the equality and inequality operators for FieldSpec.
    
    Note to John, I did not update FieldSpec::operator< because
    I wasn't sure how you'd want it.  So, it currently does nothing
    with stateArch.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@499 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 774b1307b5b2a70f8ce45aaa434cb3d8d625ff14

--- io/Field.cxx
+++ io/Field.cxx
@@ -11,10 +11,11 @@ namespace Field_NS {
 /**
  * FieldSpec Implementation
  */
-FieldSpec::FieldSpec(FieldType t, FieldLength len, const string& label) : type(t), length(len), label(label){}
-FieldSpec::FieldSpec(const FieldSpec& c) : type(c.getType()), length(c.getLength()), label(c.getLabel()) {}
+FieldSpec::FieldSpec(FieldType t, FieldLength len, const string& label) : type(t), length(len), stateArch(STATELESS), label(label){}
+FieldSpec::FieldSpec(FieldType t, FieldLength len, FieldStateArchitecture arch, const string& label) : type(t), length(len), stateArch(arch), label(label){}
+FieldSpec::FieldSpec(const FieldSpec& c) : type(c.getType()), length(c.getLength()), stateArch(c.getStateArchitecture()), label(c.getLabel()) {}
 bool FieldSpec::operator == (const FieldSpec& right) const {
-	return (type == right.getType() && length == right.getLength()) ? true : false;
+  return (type == right.getType() && length == right.getLength() && stateArch == right.getStateArchitecture()) ? true : false;
 }
 bool FieldSpec::operator != (const FieldSpec& right) const {
 	return !(*this==right);
@@ -30,6 +31,7 @@ bool FieldSpec::operator < (const FieldSpec& right) const {
 }
 FieldSpec::FieldType FieldSpec::getType() const { return type; }
 FieldSpec::FieldLength FieldSpec::getLength() const { return length; }
+FieldSpec::FieldStateArchitecture FieldSpec::getStateArchitecture() const { return stateArch; }
 string FieldSpec::getLabel() const { return label; }
 
 Field<double> getVOLUME(shared_ptr<double> data, int numPoints) {
