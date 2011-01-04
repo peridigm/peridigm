0f05f7c67796a0f597ceb9c2a64bda53838ec2e1
John Mitchell <jamitch@sandia.gov>
2011-01-04 17:40:26 +0000

    Yesterday, I was trying to run the expanding ring.  I made a mistake in the xml input deck for a fairly large mesh and it brought my machine down. We all agree that initial conditions in Peridigm are a 'hack'.  So I am proposing the following:
    This is a sketelon outline of what an initial condition 
    could look like in Peridigm.  Feedback is requested.
    
    Basic Idea:
    * Abstract InitialCondition class requires method "apply"
    * Each new kind of initial condition simply overloads this method
    * At the minimum, all initial conditions will be constructed using ParameterList peridigmParams
    as an argument.
    * Perhaps constructor should also take initialX, and pointers to 'velocity' and or something else 
    that has access to all of the data arrays
    * The 'InitialCondition' object is throw away.  We build it at the start, apply the 
    initial conditions and then thow it away.
    
    Example input for Q2Cylinder: take a look at src/initial_conditions/expanding_ring_IC.xml
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@561 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: b3170ab4f1b5a25b5d6ba382796ebafbaa2704f4

---
+++ src/initial_conditions/Q2Cylinder.hpp
@@ -0,0 +1,28 @@
+/*
+ * Q2Cylinder.hpp
+ *
+ *  Created on: Jan 4, 2011
+ *      Author: jamitch
+ */
+
+#ifndef Q2CYLINDER_HPP_
+#define Q2CYLINDER_HPP_
+
+#include "InitialCondition.hpp"
+
+namespace PeridigmNS {
+
+namespace InitialConditionsNS {
+
+class Q2Cylinder : public InitialCondition {
+public:
+	virtual ~Q2Cylinder() {}
+	Q2Cylinder(const Teuchos::RCP<Teuchos::ParameterList>& peridigmParams) {}
+	virtual void apply() {}
+};
+
+}  // namespace InitialConditionsNS
+
+} // namespace PeridigmNS
+
+#endif /* Q2CYLINDER_HPP_ */
