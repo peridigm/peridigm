af8e2e87b1d454000b9f01100e43d1a95c6bfe89
David Littlewood <djlittl@sandia.gov>
2011-01-06 22:27:55 +0000

    Added parallel unit test for rebalance.
    This version is just a placeholder, real version forthcoming.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@571 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 5326c98e7ddb97a86ca92d24727b2abe05a00df5

---
+++ src/utPeridigm_MPI_np2.cpp
@@ -0,0 +1,180 @@
+/*! \file utPeridigm_MPI_np2.cpp */
+
+// ***********************************************************************
+//
+//                             Peridigm
+//                 Copyright (2009) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// 
+// Questions? 
+// David J. Littlewood   djlittl@sandia.gov 
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ***********************************************************************
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_ALTERNATIVE_INIT_API
+#include <boost/test/unit_test.hpp>
+#include <Epetra_ConfigDefs.h> // used to define HAVE_MPI
+#include <Epetra_MpiComm.h>
+#include "Peridigm.hpp"
+
+using namespace boost::unit_test;
+using namespace Teuchos;
+using namespace PeridigmNS;
+
+Teuchos::RCP<PeridigmNS::Peridigm> createTwoPointModel()
+{
+  int rank, numProcs;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+
+  Teuchos::RCP<Epetra_Comm> comm;
+  comm = rcp(new Epetra_MpiComm(MPI_COMM_WORLD));
+
+  // set up parameter lists
+  // these data would normally be read from an input xml file
+  Teuchos::RCP<Teuchos::ParameterList> peridigmParams = rcp(new Teuchos::ParameterList());
+
+  // problem parameters
+  ParameterList& problemParams = peridigmParams->sublist("Problem");
+  problemParams.set("Verbose", false);
+
+  // material parameters
+  ParameterList& materialParams = problemParams.sublist("Material");
+  ParameterList& linearElasticMaterialParams = materialParams.sublist("Linear Elastic");
+  linearElasticMaterialParams.set("Density", 7800.0);
+  linearElasticMaterialParams.set("Bulk Modulus", 130.0e9);
+  linearElasticMaterialParams.set("Shear Modulus", 78.0e9);
+
+  // discretization parameters
+  ParameterList& discretizationParams = problemParams.sublist("Discretization");
+  discretizationParams.set("Type", "PdQuickGrid");
+  discretizationParams.set("Horizon", 2.0);
+
+  // pdQuickGrid tensor product mesh generator parameters
+  ParameterList& pdQuickGridParams = discretizationParams.sublist("TensorProduct3DMeshGenerator");
+  pdQuickGridParams.set("Type", "PdQuickGrid");
+  pdQuickGridParams.set("X Origin", -2.0);
+  pdQuickGridParams.set("Y Origin", -0.5);
+  pdQuickGridParams.set("Z Origin", -0.5);
+  pdQuickGridParams.set("X Length",  4.0);
+  pdQuickGridParams.set("Y Length",  1.0);
+  pdQuickGridParams.set("Z Length",  1.0);
+  pdQuickGridParams.set("Number Points X", 2);
+  pdQuickGridParams.set("Number Points Y", 1);
+  pdQuickGridParams.set("Number Points Z", 1);
+
+  // boundary conditions
+  ParameterList& bcParams = problemParams.sublist("Boundary Conditions");
+  // node sets
+  // these sets associate a name with a list of node ids, stored as a string
+  // in this case there's only one node per set
+  bcParams.set("Min X Node Set", "0");
+  bcParams.set("Max X Node Set", "1");
+  // initial velocity boundary conditions
+  // each boundary condition is associated with a node set, defined above
+  ParameterList& initialVelocityMinXFace = bcParams.sublist("Initial Velocity Min X Face");
+  initialVelocityMinXFace.set("Type", "Initial Velocity");
+  initialVelocityMinXFace.set("Node Set", "Min X Node Set");
+  initialVelocityMinXFace.set("Coordinate", "x");
+  initialVelocityMinXFace.set("Value", -1.0);
+  ParameterList& initialVelocityMaxXFace = bcParams.sublist("Initial Velocity Max X Face");
+  initialVelocityMaxXFace.set("Type", "Initial Velocity");
+  initialVelocityMaxXFace.set("Node Set", "Max X Node Set");
+  initialVelocityMaxXFace.set("Coordinate", "x");
+  initialVelocityMaxXFace.set("Value", 1.0);
+
+  // solver parameters
+  ParameterList& solverParams = peridigmParams->sublist("Solver");
+  solverParams.set("Verbose", "false");
+  ParameterList& verletParams = solverParams.sublist("Verlet");
+  verletParams.set("Initial Time", 0.0);
+  verletParams.set("Final Time", 1.0);
+  verletParams.set("Fixed dt", 1.0);
+
+  // create the Peridigm object
+  Teuchos::RCP<PeridigmNS::Peridigm> peridigm = rcp(new PeridigmNS::Peridigm(comm, peridigmParams));
+
+  return peridigm;
+}
+
+
+void initialize()
+{
+  Teuchos::RCP<PeridigmNS::Peridigm> peridigm = createTwoPointModel();
+
+  // \todo Write meaningful asserts.
+}
+
+void simpleTensorProductMesh()
+{
+  int rank, numProcs;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+
+  Teuchos::RCP<Epetra_Comm> comm;
+  comm = rcp(new Epetra_MpiComm(MPI_COMM_WORLD));
+
+
+}
+
+bool init_unit_test_suite()
+{
+	// Add a suite for each processor in the test
+	bool success = true;
+
+	test_suite* proc = BOOST_TEST_SUITE("utPeridigm__MPI_np2");
+	proc->add(BOOST_TEST_CASE(&initialize));
+    //	proc->add(BOOST_TEST_CASE(&rebalance));
+	framework::master_test_suite().add(proc);
+
+	return success;
+}
+
+bool init_unit_test()
+{
+	init_unit_test_suite();
+	return true;
+}
+
+int main
+(int argc, char* argv[])
+{
+  MPI_Init(&argc,&argv);
+  int numProcs;
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+
+  int return_code = -1;
+
+  if(numProcs == 2){
+	return_code = unit_test_main(init_unit_test, argc, argv);
+  }
+  else{
+	std::cerr << "Unit test runtime ERROR: utPeridigm_MPI_np2 only makes sense on 2 processors" << std::endl;
+	std::cerr << "Re-run unit test $mpiexec -np 2 ./utPeridigm_MPI_np2" << std::endl;
+  }
+
+  MPI_Finalize();
+
+  return return_code;
+}
