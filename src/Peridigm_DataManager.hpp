590599ed1ce46ab38195b9d65b432cfdd5e7ee35
David Littlewood <djlittl@sandia.gov>
2011-02-09 04:45:11 +0000

    Added doxygen comments for State and DataManager.
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@624 d9297710-12f9-482b-b11f-fb677dc88bbb

--- src/Peridigm_DataManager.hpp
+++ src/Peridigm_DataManager.hpp
@@ -39,16 +39,40 @@
 
 namespace PeridigmNS {
 
+/*! \brief A lean, mean, data managing machine.
+ *
+ * DataManager is a container class for managing the data required for peridynamic force evaluations. 
+ * Data is stored in State objects which may be either stateless (STATE_NONE) or stateful (STATE_N, STATE_NP1).
+ * Three types of data are supported:  scalar, vector, and bond data.  In the cases of scalar and vector data,
+ * the DataManger class has notions of owned points and ghosted points, allowing force calcuations access
+ * to off-processor points that are within the neighborhood of one or more owned points.
+ * Data is accessed via the getData function, which provides access to the Epetra_Vector corresponding to
+ * the given FieldSpec and FieldStep.
+ */
 class DataManager {
 
 public:
-
+  
+  //! Constructor.
   DataManager() : rebalanceCount(0) {}
-  DataManager(const DataManager& dataManager){
-    // \todo Write me.
-  }
+
+  //! Copy constructor.
+  DataManager(const DataManager& dataManager){}
+
+  //! Destructor.
   ~DataManager(){}
 
+  /*! \brief Sets the maps, must be called prior to allocating data.
+   *
+   * Sets the following maps:
+   * <ul>
+   * <li> ownedIDScalarMap:  Identifies the points owned by the calling processor.
+   * <li> ownedIDVectorMap:  Identical to ownedIDScalarMap, but with dimension three.
+   * <li> scalarMap:         One-dimensional overlap map containing the owned points and ghosts.
+   * <li> vectorMap:         Three-dimensional overlap map containing the owned points and ghosts.
+   * <li> bondMap:           Map containing one element for each owned point; the length of the element is equal to the number of bonds.
+   * </ul>
+   */
   void setMaps(Teuchos::RCP<const Epetra_BlockMap> ownedIDScalarMap_,
                Teuchos::RCP<const Epetra_BlockMap> ownedIDVectorMap_,
                Teuchos::RCP<const Epetra_BlockMap> scalarMap_,
@@ -61,53 +85,77 @@ public:
     bondMap = bondMap_;
   }
 
+  //! Instantiates State objects corresponding to the given list of FieldSpecs. 
   void allocateData(Teuchos::RCP< std::vector<Field_NS::FieldSpec> > specs);
 
-  //! For each global ID, copies data from the processor that owns the global ID to processors that have ghosted copies.
-  //  This ensures that all processors that have a copy of a given element have the same data values for that element.
+  //! For each point, copies data from the processor that owns the point to processors that have ghosted copies.
   void scatterToGhosts();
 
+  //! Redistributes data based on new partitioning as defined by given maps.
   void rebalance(Teuchos::RCP<const Epetra_BlockMap> rebalancedOwnedIDScalarMap,
                  Teuchos::RCP<const Epetra_BlockMap> rebalancedOwnedIDVectorMap,
                  Teuchos::RCP<const Epetra_BlockMap> rebalancedScalarMap,
                  Teuchos::RCP<const Epetra_BlockMap> rebalancedVectorMap,
                  Teuchos::RCP<const Epetra_BlockMap> rebalancedBondMap);
 
+  //! Returns the number of times rebalance has been called.
   int getRebalanceCount(){ return rebalanceCount; }
 
+  //! Provides access to the Epetra_Vector specified by the given FieldSped and FieldStep.
   Teuchos::RCP<Epetra_Vector> getData(Field_NS::FieldSpec fieldSpec,
                                       Field_NS::FieldSpec::FieldStep fieldStep);
 
+  //! Swaps StateN and StateNP1; stateNONE is unaffected.
   void updateState(){
     stateN.swap(stateNP1);
   }
 
 protected:
 
+  //! Number of times rebalance has been called.
   int rebalanceCount;
+
+  //! @name Field specifications
+  //@{
+  //! Complete list of field specs.
   Teuchos::RCP< std::vector<Field_NS::FieldSpec> > fieldSpecs;
+  //! Field specs for stateless scalar data.
   Teuchos::RCP< std::vector<Field_NS::FieldSpec> > statelessScalarFieldSpecs;
+  //! Field specs for stateless vector data.
   Teuchos::RCP< std::vector<Field_NS::FieldSpec> > statelessVectorFieldSpecs;
+  //! Field specs for stateless bond data.
   Teuchos::RCP< std::vector<Field_NS::FieldSpec> > statelessBondFieldSpecs;
+  //! Field specs for stateful scalar data.
   Teuchos::RCP< std::vector<Field_NS::FieldSpec> > statefulScalarFieldSpecs;
+  //! Field specs for stateful vector data.
   Teuchos::RCP< std::vector<Field_NS::FieldSpec> > statefulVectorFieldSpecs;
+  //! Field specs for stateful bond data.
   Teuchos::RCP< std::vector<Field_NS::FieldSpec> > statefulBondFieldSpecs;
+  //@}
 
-  // scalar and vector data is stored in overlapped vectors
-  // this allows for easy indexing in the material models
+  //! @name Maps
+  //@{
+  //! One-dimensional map for owned points.
   Teuchos::RCP<const Epetra_BlockMap> ownedIDScalarMap;
+  //! Three-dimensional map for owned points.
   Teuchos::RCP<const Epetra_BlockMap> ownedIDVectorMap;
+  //! One-dimensional overlap map for owned points and ghosts.
   Teuchos::RCP<const Epetra_BlockMap> scalarMap;
+  //! Three-dimensional overlap map for owned points and ghosts.
   Teuchos::RCP<const Epetra_BlockMap> vectorMap;
-
-  // the bond map is not overlapping
-  // it follows the convensions of neighborhood data
+  //! Bond map for owned points; the map contains one element for each owned point, the element length is equal to the number of bonds for that point.
   Teuchos::RCP<const Epetra_BlockMap> bondMap;
+  //@}
 
-  // data is stored in state objects
+  //! @name State objects
+  //@{
+  //! Data storage for state N.
   Teuchos::RCP<State> stateN;
+  //! Data storage for state N plus 1.
   Teuchos::RCP<State> stateNP1;
+  //! Data storage for state NONE (stateless data).
   Teuchos::RCP<State> stateNONE;
+  //@}
 };
 
 }
