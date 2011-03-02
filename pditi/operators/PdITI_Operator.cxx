d0edf938a53b8949d61399326a09b24437663ad3
John Mitchell <jamitch@sandia.gov>
2011-03-02 22:39:05 +0000

    Incremental checkin on new PdITI operator. Its now used in the one unit test below along side the existing operator.  Test passes.
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@650 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 232f4c55f73104159a12a9c51ee51e13d09610f1

--- pditi/operators/PdITI_Operator.cxx
+++ pditi/operators/PdITI_Operator.cxx
@@ -9,7 +9,9 @@
 #include "Epetra_BlockMap.h"
 #include "Epetra_Vector.h"
 #include "PdMaterialUtilities.h"
-
+#include <iostream>
+using std::cout;
+using std::endl;
 using namespace PdMaterialUtilities;
 namespace PdITI {
 
@@ -53,7 +55,6 @@ temporalBondFields()
 	Epetra_Vector xOwned(View,ownedMapNDF,list.get_owned_x().get());
 	xOverlap.Import(xOwned,importNDF,Insert);
 
-
 	/*
 	 * Pull in shared volume
 	 * NOTE View versions of Epetra_Vectors
@@ -68,5 +69,158 @@ temporalBondFields()
 	computeWeightedVolume(xOverlapPtr.get(),volumeOverlapPtr.get(),mOwnedField.getArray().get(),neighborhoodList.get_num_owned_points(),list.get_neighborhood().get());
 
 }
+
+Field_NS::Field<double> PdITI_Operator::computeOwnedDilatation(Field_NS::Field<double> uOwnedField) {
+
+	/*
+	 * UPDATE GEOMETRY FOR GIVEN DISPLACMENT FIELD
+	 *
+	 *
+	 * Broadcast  displacement to sharing processors
+	 * Note VIEW Variety of Epetra_Vectors
+	 */
+	Epetra_Vector uOverlap(View,overlapMapNDF,uOverlapPtr.get());
+	Epetra_Vector uOwned(View,ownedMapNDF,uOwnedField.getArray().get());
+	uOverlap.Import(uOwned,importNDF,Insert);
+
+	double *x = xOverlapPtr.get();
+	double *u = uOverlapPtr.get();
+	double *y = yOverlapPtr.get();
+	double *end = x+overlapMapNDF.NumMyPoints();
+	for(;x!=end;x++,u++,y++){
+		*y = *x + *u;
+	}
+
+	shared_ptr<int> localNeighborList = list.get_neighborhood();
+	int numOwnedPoints = list.get_num_owned_points();
+	/*
+	 *
+	 * COMPUTE DILATATION
+	 */
+	double *theta = dilatationOwnedField.getArray().get();
+
+	computeDilatation(xOverlapPtr.get(),yOverlapPtr.get(),mOwnedField.getArray().get(),volumeOverlapPtr.get(),bondDamage.get(),theta,localNeighborList.get(),numOwnedPoints);
+
+	return dilatationOwnedField;
+}
+
+/**
+ * Assembles into the fIntOwnedField
+ */
+void PdITI_Operator::computeInternalForce(Field_NS::Field<double> uOwnedField, Field_NS::Field<double> fIntOwnedField, bool withDilatation) {
+	/*
+	 * UPDATE GEOMETRY FOR GIVEN DISPLACMENT FIELD
+	 *
+	 *
+	 * Broadcast  displacement to sharing processors
+	 * Note VIEW Variety of Epetra_Vectors
+	 */
+	Epetra_Vector uOverlap(View,overlapMapNDF,uOverlapPtr.get());
+	Epetra_Vector uOwned(View,ownedMapNDF,uOwnedField.getArray().get());
+	uOverlap.Import(uOwned,importNDF,Insert);
+
+	double *x = xOverlapPtr.get();
+	double *u = uOverlapPtr.get();
+	double *y = yOverlapPtr.get();
+	double *f = fInternalOverlapPtr.get();
+	double *end = x+overlapMapNDF.NumMyPoints();
+	for(;x!=end;x++,u++,y++,f++){
+		*y = *x + *u;
+		*f = 0;
+	}
+
+	shared_ptr<int> localNeighborList = list.get_neighborhood();
+	int numOwnedPoints = list.get_num_owned_points();
+	/*
+	 *
+	 * COMPUTE DILATATION
+	 */
+	/*
+	 * THIS IS KIND OF A HACK for developing the plasticity model
+	 */
+	double *theta = dilatationOwnedField.getArray().get();
+	if(withDilatation){
+		computeDilatation(xOverlapPtr.get(),yOverlapPtr.get(),mOwnedField.getArray().get(),volumeOverlapPtr.get(),bondDamage.get(),theta,localNeighborList.get(),numOwnedPoints);
+	} else {
+		dilatationOwnedField.setValue(0.0);
+	}
+
+	/*
+	 * Create array of pointers to temporal bond variables
+	 */
+//	vector<double*> temporalBondVarsN   = vector<double*>(temporalBondFields.size());
+//	vector<double*> temporalBondVarsNp1 = vector<double*>(temporalBondFields.size());
+//	for(std::size_t v=0;v<temporalBondVars.size();v++){
+//		Field<double> fieldN = temporalBondFields[v].getField(Field_NS::FieldSpec::STEP_N);
+//		temporalBondVarsN[v] = fieldN.getArray().get();
+//		Field<double> fieldNP1 = temporalBondFields[v].getField(Field_NS::FieldSpec::STEP_NP1);
+//		temporalBondVarsNp1[v] = fieldNP1.getArray().get();
+//	}
+
+
+	/*
+	 * COMPUTE INTERNAL FORCE
+	 */
+
+	fIntPtr->computeInternalForce
+	(
+			xOverlapPtr.get(),
+			yOverlapPtr.get(),
+			mOwnedField.getArray().get(),
+			volumeOverlapPtr.get(),
+			theta,
+			bondDamage.get(),
+			fInternalOverlapPtr.get(),
+			localNeighborList.get(),
+			numOwnedPoints,
+			temporalBondFields
+	);
+
+	/*
+	 * ASSEMBLE COMPUTED FORCES across all processors
+	 */
+	Epetra_Vector fOverlap(View,overlapMapNDF,fInternalOverlapPtr.get());
+	Epetra_Vector fOwned(View,ownedMapNDF,fIntOwnedField.getArray().get());
+
+	/*
+	 * Export "fOverlap" to fOwned
+	 */
+	fOwned.Export(fOverlap,exportAssembly,Add);
+
+}
+
+void PdITI_Operator::addConstitutiveModel(shared_ptr<PdITI::ConstitutiveModel>& model) {
+
+	fIntPtr = model;
+	vector<FieldSpec> temporalBondSpecs = fIntPtr->registerTemporalBondVariables();
+	std::size_t numPoints = list.get_num_owned_points();
+	std::size_t allocateBondVarSize = list.get_size_neighborhood_list() - numPoints;
+
+	/*
+	 * Create and allocate temporal bond fields
+	 */
+	temporalBondFields = vector<TemporalField<double> >(temporalBondSpecs.size());
+	for(std::size_t f=0;f<temporalBondSpecs.size();f++){
+		temporalBondFields[f] = TemporalField<double>(temporalBondSpecs[f],allocateBondVarSize);
+		Field<double> fN = temporalBondFields[f].getField(FieldSpec::STEP_N);
+		Field<double> fNp1 = temporalBondFields[f].getField(FieldSpec::STEP_NP1);
+		fN.setValue(0.0);
+		fNp1.setValue(0.0);
+
+	}
+
+}
+
+void PdITI_Operator::advanceStateVariables() {
+
+	/*
+	 * Loop over temporal fields and advance state
+	 */
+	for(std::size_t f=0;f<temporalBondFields.size();f++){
+		temporalBondFields[f].advanceStep();
+	}
+
+}
+
 
 }
--- pditi/operators/PdITI_Operator.h
+++ pditi/operators/PdITI_Operator.h
@@ -24,6 +24,16 @@ namespace PdITI {
 class PdITI_Operator {
 public:
 	PdITI_Operator(const Epetra_Comm& comm, const PDNEIGH::NeighborhoodList neighborhoodList, shared_ptr<double> ownedCellVolume);
+	void addConstitutiveModel(shared_ptr<ConstitutiveModel>& model);
+	void advanceStateVariables();
+	Field_NS::Field<double> computeOwnedDilatation(Field_NS::Field<double> uOwnedField);
+	void computeInternalForce(Field_NS::Field<double> uOwned, Field_NS::Field<double> fIntOwned, bool withDilatation=true);
+	Field_NS::Field<double> getWeightedVolume() const { return mOwnedField; }
+	const Epetra_Comm& getEpetra_Comm() const { return epetraComm; }
+	const Epetra_BlockMap& getOwnedMapScalar() { return  ownedMapScalar; }
+	const Epetra_BlockMap& getOverlapMapScalar() { return  overlapMapScalar; }
+	const Epetra_BlockMap& getOwnedMapNDF()   const { return  ownedMapNDF; }
+	const Epetra_BlockMap& getOverlapMapNDF() const { return  overlapMapNDF; }
 
 private:
 	const Epetra_Comm& epetraComm;
