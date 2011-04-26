63987e2a4cac36bbf285397c599eb33bc96fe6d1
David Littlewood <djlittl@sandia.gov>
2011-04-26 18:09:38 +0000

    Incremental check-in toward refactoring of finite-difference Jacobian to use a temporary data manager.
    Works in serial, fails in parallel, code is very messy.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@774 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 0e76ff54e3ad6c09e7877caf0f58624190ab81ab

--- src/materials/Peridigm_Material.cpp
+++ src/materials/Peridigm_Material.cpp
@@ -47,6 +47,7 @@
 
 #include "Peridigm_Material.hpp"
 #include <Teuchos_TestForException.hpp>
+#include <Epetra_SerialComm.h>
 
 using namespace std;
 
@@ -57,11 +58,6 @@ void PeridigmNS::Material::computeJacobian(const double dt,
                                            PeridigmNS::DataManager& dataManager,
                                            PeridigmNS::SerialMatrix& jacobian) const
 {
-  // Extract pointers to the underlying data in the constitutiveData array
-  double *y, *force;
-  dataManager.getData(Field_NS::CURCOORD3D, Field_NS::FieldSpec::STEP_NP1)->ExtractView(&y);
-  dataManager.getData(Field_NS::FORCE_DENSITY3D, Field_NS::FieldSpec::STEP_NP1)->ExtractView(&force);
-
   // The Jacobian is of the form:
   //
   // dF_0x/dx_0  dF_0x/dy_0  dF_0x/dz_0  dF_0x/dx_1  dF_0x/dy_1  dF_0x/dz_1  ...  dF_0x/dx_n  dF_0x/dy_n  dF_0x/dz_n  
@@ -83,30 +79,166 @@ void PeridigmNS::Material::computeJacobian(const double dt,
 
   double epsilon = 1.0e-6; // \todo Instead, use 1.0e-6 * smallest_radius_in_model
 
-  // Create a temporary vector for storing the unperturbed force
-  Teuchos::RCP<Epetra_Vector> forceVector = dataManager.getData(Field_NS::FORCE_DENSITY3D, Field_NS::FieldSpec::STEP_NP1);
-  Teuchos::RCP<Epetra_Vector> unperturbedForceVector = Teuchos::rcp(new Epetra_Vector(*forceVector));
-  double* unperturbedForce;
-  unperturbedForceVector->ExtractView(&unperturbedForce);
-
   // loop over all points
   int neighborhoodListIndex = 0;
   for(int iID=0 ; iID<numOwnedPoints ; ++iID){
 
-    // create a neighborhood consisting of a single point and its neighbors
+    // create a temporary neighborhood consisting of a single point and its neighbors
     int numNeighbors = neighborhoodList[neighborhoodListIndex++];
-    int tempNumOwnedPoints = 1;
-    int* tempOwnedIDs = &iID;
+    vector<int> tempMyGlobalIDs(numNeighbors+1);
+    tempMyGlobalIDs[0] = dataManager.getOverlapIDScalarMap()->GID(iID);
     vector<int> tempNeighborhoodList(numNeighbors+1); 
     tempNeighborhoodList[0] = numNeighbors;
     for(int iNID=0 ; iNID<numNeighbors ; ++iNID){
       int neighborID = neighborhoodList[neighborhoodListIndex++];
-      tempNeighborhoodList[iNID+1] = neighborID;
+      tempMyGlobalIDs[iNID+1] = dataManager.getOverlapIDScalarMap()->GID(neighborID);
+      tempNeighborhoodList[iNID+1] = iNID+1;
     }
 
+    Epetra_SerialComm serialComm;
+    Teuchos::RCP<Epetra_BlockMap> tempOneDimensionalMap = Teuchos::rcp(new Epetra_BlockMap(numNeighbors+1, numNeighbors+1, &tempMyGlobalIDs[0], 1, 0, serialComm));
+    Teuchos::RCP<Epetra_BlockMap> tempThreeDimensionalMap = Teuchos::rcp(new Epetra_BlockMap(numNeighbors+1, numNeighbors+1, &tempMyGlobalIDs[0], 3, 0, serialComm));
+    Teuchos::RCP<Epetra_BlockMap> tempBondMap = Teuchos::rcp(new Epetra_BlockMap(1, 1, &tempMyGlobalIDs[0], numNeighbors, 0, serialComm));
+
+    // create a temporary DataManager containing data for this point and its neighborhood
+    PeridigmNS::DataManager tempDataManager;
+    tempDataManager.setMaps(Teuchos::RCP<const Epetra_BlockMap>(),
+                            Teuchos::RCP<const Epetra_BlockMap>(),
+                            tempOneDimensionalMap,
+                            tempThreeDimensionalMap,
+                            tempBondMap);
+
+    // the temporary data manager will have the same field specs as the real data manager
+    Teuchos::RCP< std::vector<Field_NS::FieldSpec> > fieldSpecs = dataManager.getFieldSpecs();
+    tempDataManager.allocateData(fieldSpecs);
+
+    // copy data into the tempDataManager
+    // \todo Can this be done with an import (parallel comm -> serial comm)?
+    // \todo Write a DataManager function to perform this copy operation.
+    for(unsigned int iFieldSpec=0 ; iFieldSpec<fieldSpecs->size() ; ++iFieldSpec){
+
+      Field_NS::FieldSpec spec = (*fieldSpecs)[iFieldSpec];
+      Field_NS::FieldSpec::FieldStateArchitecture arch = spec.getStateArchitecture();
+      Field_NS::FieldSpec::FieldLength length = spec.getLength();
+
+      // for scalar and vector data, copy data for the given point and all its neighbors
+      if(length == Field_NS::FieldSpec::SCALAR || length == Field_NS::FieldSpec::VECTOR3D){
+
+        if(arch == Field_NS::FieldSpec::STATELESS){
+          Epetra_Vector& source = *(dataManager.getData(spec, Field_NS::FieldSpec::STEP_NONE));
+          Epetra_Vector& target = *(tempDataManager.getData(spec, Field_NS::FieldSpec::STEP_NONE));
+          for(unsigned int i=0 ; i<tempMyGlobalIDs.size() ; ++i){
+            int globalID = tempMyGlobalIDs[i];
+            int sourceLocalID = source.Map().LID(globalID);
+            int targetLocalID = target.Map().LID(globalID);
+            if(spec.getLength() == Field_NS::FieldSpec::SCALAR){
+              target[targetLocalID] = source[sourceLocalID];
+            }
+            else if(spec.getLength() == Field_NS::FieldSpec::VECTOR3D){
+              target[3*targetLocalID] = source[3*sourceLocalID];
+              target[3*targetLocalID+1] = source[3*sourceLocalID+1];
+              target[3*targetLocalID+2] = source[3*sourceLocalID+2];
+            }
+          }
+        }
+        else if(arch == Field_NS::FieldSpec::STATEFUL){
+          {
+            Epetra_Vector& source = *(dataManager.getData(spec, Field_NS::FieldSpec::STEP_N));
+            Epetra_Vector& target = *(tempDataManager.getData(spec, Field_NS::FieldSpec::STEP_N));
+            for(unsigned int i=0 ; i<tempMyGlobalIDs.size() ; ++i){
+              int globalID = tempMyGlobalIDs[i];
+              int sourceLocalID = source.Map().LID(globalID);
+              int targetLocalID = target.Map().LID(globalID);
+              if(spec.getLength() == Field_NS::FieldSpec::SCALAR){
+                target[targetLocalID] = source[sourceLocalID];
+              }
+              else if(spec.getLength() == Field_NS::FieldSpec::VECTOR3D){
+                target[3*targetLocalID] = source[3*sourceLocalID];
+                target[3*targetLocalID+1] = source[3*sourceLocalID+1];
+                target[3*targetLocalID+2] = source[3*sourceLocalID+2];
+              }
+            }
+          }
+          {
+            Epetra_Vector& source = *(dataManager.getData(spec, Field_NS::FieldSpec::STEP_NP1));
+            Epetra_Vector& target = *(tempDataManager.getData(spec, Field_NS::FieldSpec::STEP_NP1));
+            for(unsigned int i=0 ; i<tempMyGlobalIDs.size() ; ++i){
+              int globalID = tempMyGlobalIDs[i];
+              int sourceLocalID = source.Map().LID(globalID);
+              int targetLocalID = target.Map().LID(globalID);
+              if(spec.getLength() == Field_NS::FieldSpec::SCALAR){
+                target[targetLocalID] = source[sourceLocalID];
+              }
+              else if(spec.getLength() == Field_NS::FieldSpec::VECTOR3D){
+                target[3*targetLocalID] = source[3*sourceLocalID];
+                target[3*targetLocalID+1] = source[3*sourceLocalID+1];
+                target[3*targetLocalID+2] = source[3*sourceLocalID+2];
+              }
+            }
+          }
+        }
+      }
+      else if(spec.getLength() == Field_NS::FieldSpec::BOND){
+
+        if(arch == Field_NS::FieldSpec::STATELESS){
+          Epetra_Vector& source = *(dataManager.getData(spec, Field_NS::FieldSpec::STEP_NONE));
+          Epetra_Vector& target = *(tempDataManager.getData(spec, Field_NS::FieldSpec::STEP_NONE));
+          // there is bond data only for the owned ID
+          int globalID = tempMyGlobalIDs[0];
+          int sourceLocalID = source.Map().LID(globalID);
+          int targetLocalID = target.Map().LID(globalID);
+          int sourceFirstPointInElement = source.Map().FirstPointInElement(sourceLocalID);
+          int targetFirstPointInElement = target.Map().FirstPointInElement(targetLocalID);
+          int elementSize = source.Map().ElementSize(sourceLocalID);
+          for(int i=0 ; i<elementSize ; ++i)
+            target[targetFirstPointInElement+i] = source[sourceFirstPointInElement+i];
+        }
+        else if(arch == Field_NS::FieldSpec::STATEFUL){
+          Epetra_Vector& source = *(dataManager.getData(spec, Field_NS::FieldSpec::STEP_N));
+          Epetra_Vector& target = *(tempDataManager.getData(spec, Field_NS::FieldSpec::STEP_N));
+          // there is bond data only for the owned ID
+          int globalID = tempMyGlobalIDs[0];
+          int sourceLocalID = source.Map().LID(globalID);
+          int targetLocalID = target.Map().LID(globalID);
+          int sourceFirstPointInElement = source.Map().FirstPointInElement(sourceLocalID);
+          int targetFirstPointInElement = target.Map().FirstPointInElement(targetLocalID);
+          int elementSize = source.Map().ElementSize(sourceLocalID);
+          for(int i=0 ; i<elementSize ; ++i)
+            target[targetFirstPointInElement+i] = source[sourceFirstPointInElement+i];
+          source = *(dataManager.getData(spec, Field_NS::FieldSpec::STEP_NP1));
+          target = *(tempDataManager.getData(spec, Field_NS::FieldSpec::STEP_NP1));
+          globalID = tempMyGlobalIDs[0];
+          sourceLocalID = source.Map().LID(globalID);
+          targetLocalID = target.Map().LID(globalID);
+          sourceFirstPointInElement = source.Map().FirstPointInElement(sourceLocalID);
+          targetFirstPointInElement = target.Map().FirstPointInElement(targetLocalID);
+          elementSize = source.Map().ElementSize(sourceLocalID);
+          for(int i=0 ; i<elementSize ; ++i)
+            target[targetFirstPointInElement+i] = source[sourceFirstPointInElement+i];
+        }
+      }
+    }
+
+    // set up numOwnedPoints and ownedIDs
+    // there is only one owned ID, and it has local ID zero in the tempDataManager
+    int tempNumOwnedPoints = 1;
+    vector<int> tempOwnedIDs(1);
+    tempOwnedIDs[0] = 0;
+
+    // Extract pointers to the underlying data in the constitutiveData array
+    double *y, *force;
+    tempDataManager.getData(Field_NS::CURCOORD3D, Field_NS::FieldSpec::STEP_NP1)->ExtractView(&y);
+    tempDataManager.getData(Field_NS::FORCE_DENSITY3D, Field_NS::FieldSpec::STEP_NP1)->ExtractView(&force);
+
+    // Create a temporary vector for storing the unperturbed force
+    Teuchos::RCP<Epetra_Vector> forceVector = tempDataManager.getData(Field_NS::FORCE_DENSITY3D, Field_NS::FieldSpec::STEP_NP1);
+    Teuchos::RCP<Epetra_Vector> unperturbedForceVector = Teuchos::rcp(new Epetra_Vector(*forceVector));
+    double* unperturbedForce;
+    unperturbedForceVector->ExtractView(&unperturbedForce);
+
     // compute and store the unperturbed force
-    updateConstitutiveData(dt, tempNumOwnedPoints, tempOwnedIDs, &tempNeighborhoodList[0], dataManager);
-    computeForce(dt, tempNumOwnedPoints, tempOwnedIDs, &tempNeighborhoodList[0], dataManager);
+    updateConstitutiveData(dt, tempNumOwnedPoints, &tempOwnedIDs[0], &tempNeighborhoodList[0], tempDataManager);
+    computeForce(dt, tempNumOwnedPoints, &tempOwnedIDs[0], &tempNeighborhoodList[0], tempDataManager);
     for(int i=0 ; i<forceVector->MyLength() ; ++i)
       unperturbedForce[i] = force[i];
 
@@ -118,15 +250,15 @@ void PeridigmNS::Material::computeJacobian(const double dt,
       if(iNID < numNeighbors)
         perturbID = tempNeighborhoodList[iNID+1];
       else
-        perturbID = iID;
+        perturbID = 0;//iID;
 
       for(int dof=0 ; dof<3 ; ++dof){
 
         // perturb a dof and compute the forces
         double oldY = y[3*perturbID+dof];
         y[3*perturbID+dof] += epsilon;
-        updateConstitutiveData(dt, tempNumOwnedPoints, tempOwnedIDs, &tempNeighborhoodList[0], dataManager);
-        computeForce(dt, tempNumOwnedPoints, tempOwnedIDs, &tempNeighborhoodList[0], dataManager);
+        updateConstitutiveData(dt, tempNumOwnedPoints, &tempOwnedIDs[0], &tempNeighborhoodList[0], tempDataManager);
+        computeForce(dt, tempNumOwnedPoints, &tempOwnedIDs[0], &tempNeighborhoodList[0], tempDataManager);
 
         // fill in the corresponding Jacobian entries
         for(int i=0 ; i<numNeighbors+1 ; ++i){
@@ -135,12 +267,16 @@ void PeridigmNS::Material::computeJacobian(const double dt,
           if(i < numNeighbors)
             forceID = tempNeighborhoodList[i+1];
           else
-            forceID = iID;
+            forceID = 0;//iID;
 
           for(int d=0 ; d<3 ; ++d){
             double value = ( force[3*forceID+d] - unperturbedForce[3*forceID+d] ) / epsilon;
-            int row = 3*forceID + d;
-            int col = 3*perturbID + dof;
+            int globalForceID = tempOneDimensionalMap->GID(forceID);
+            int globalPerturbID = tempOneDimensionalMap->GID(perturbID);
+            int localForceID = dataManager.getOwnedIDScalarMap()->LID(globalForceID);
+            int localPerturbID = dataManager.getOwnedIDScalarMap()->LID(globalPerturbID);
+            int row = 3*localForceID + d;
+            int col = 3*localPerturbID + dof;
             jacobian.addValue(row, col, value);
           }
         }
