1df8d5cb11caebfbceb36f2e00d2ad8157a0c912
Michael Parks <mlparks@sandia.gov>
2011-05-16 14:53:12 +0000

    Race condition apparently continues on the test machines. I'm removing the code to generate the compute header file, and interting one manually.
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@818 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 1d9da8f7729f7ad4c180621e38009e8ef76c14b6

---
+++ src/compute/compute_includes.hpp
@@ -0,0 +1,4 @@
+#include "./compute/Peridigm_Compute_Acceleration.hpp"
+#include "./compute/Peridigm_Compute_Contact_Force.hpp"
+#include "./compute/Peridigm_Compute_Force.hpp"
+#include "./compute/Peridigm_Compute.hpp"
--- src/compute/compute_includes.sh
+++
@@ -1,9 +0,0 @@
-# Produce compute_includes.hpp file
-
-rm -f compute_includes.hpp
-list=`ls *.hpp`
-for file in $list; do
-  qfile="$file"
-  echo "#include \"""./compute/"$file"\"" >> compute_includes.hpp
-done
-
