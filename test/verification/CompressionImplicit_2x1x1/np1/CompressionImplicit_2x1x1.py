a60d13f0931797ad6052a11302f836c04ef5c824
Michael Parks <mlparks@sandia.gov>
2011-06-10 05:26:37 +0000

    Further incremental work towards implicit time integration.
    Checking in first verification test, consisting of 2 particles being advanced in time
    by Newmark-Beta. 
    
    Still a few features left to add to time integrator.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@825 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: c3cfe13c104af16bad52fed391807c91be27142d

---
+++ test/verification/CompressionImplicit_2x1x1/np1/CompressionImplicit_2x1x1.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import re
+from subprocess import Popen
+
+test_dir = "CompressionImplicit_2x1x1/np1"
+base_name = "CompressionImplicit_2x1x1"
+
+if __name__ == "__main__":
+
+    result = 0
+
+    # log file will be dumped if verbose option is given
+    verbose = False
+    if "-verbose" in sys.argv:
+        verbose = True
+
+    # change to the specified test directory
+    os.chdir(test_dir)
+
+    # open log file
+    log_file_name = base_name + ".log"
+    if os.path.exists(log_file_name):
+        os.remove(log_file_name)
+    logfile = open(log_file_name, 'w')
+
+    # remove old output files, if any
+    # use regular expression module since differentiating
+    # between gold files and old output files can be tricky
+    files_to_remove = []
+    for file in os.listdir(os.getcwd()):
+        vals = re.split("\_t[0123456789]*\.vtu", file)
+        if len(vals) > 1:
+            vtu_basename = re.split("\.p[0123456789]*", vals[0])[0]
+            if vtu_basename == base_name:
+                files_to_remove.append(file)
+        vals = re.split("\_t[0123456789]*\.pvtu", file)
+        if len(vals) > 1:
+            pvtu_basename = re.split("\.p[0123456789]*", vals[0])[0]
+            if pvtu_basename == base_name:
+                files_to_remove.append(file)
+    for file in files_to_remove:
+        os.remove(file)
+
+    # run Peridigm
+    command = ["../../../../src/Peridigm", "../"+base_name+".xml"]
+    p = Popen(command, stdout=logfile, stderr=logfile)
+    return_code = p.wait()
+    if return_code != 0:
+        result = return_code
+
+    # compare output files against gold files
+    command = ["../../../../scripts/vtkDiff.py", \
+                   base_name+".pvd", \
+                   base_name+"_gold.pvd", \
+                   "--toleranceFile", \
+                   "../"+base_name+".comp"]
+    p = Popen(command, stdout=logfile, stderr=logfile)
+    return_code = p.wait()
+    if return_code != 0:
+        result = return_code
+
+    logfile.close()
+
+    # dump the output if the user requested verbose
+    if verbose == True:
+        os.system("cat " + log_file_name)
+
+    sys.exit(result)
