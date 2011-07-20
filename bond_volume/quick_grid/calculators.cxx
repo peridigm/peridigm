439ee843429ca5ee5200404ec56926db743c3cd4
John Mitchell <jamitch@sandia.gov>
2011-07-20 19:46:34 +0000

    Added a new constructor to Vector3D which then allows for initialization of vectors in the bond volume calculator.
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@901 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: c7cbb6c3989e12c48c6ed41dc0c143767a4eab17

--- bond_volume/quick_grid/calculators.cxx
+++ bond_volume/quick_grid/calculators.cxx
@@ -15,8 +15,7 @@ namespace VOLUME_FRACTION {
 using UTILITIES::Array;
 
 double RingVolumeFractionCalculator::cellVolume(const double* q) const {
-	Vector3D Q;
-        Q[0] = q[0]; Q[1] = q[1]; Q[2] = q[2];
+	Vector3D Q(*q,*(q+1),*(q+2));
 	Array<double> r, theta, z;
 	double dr, d_theta, dz;
 	{
@@ -72,9 +71,8 @@ double RingVolumeFractionCalculator::cellVolume(const double* q) const {
 
 double RingVolumeFractionCalculator::operator() (const double* pCenter, const double* qNeigh) const {
 
-	Vector3D P,Q;
-        P[0] = pCenter[0]; P[1] = pCenter[1]; P[2] = pCenter[2];
-        Q[0] = qNeigh[0]; Q[1] = qNeigh[1]; Q[2] = qNeigh[2];
+	const Vector3D P(*pCenter,*(pCenter+1),*(pCenter+2));
+	const Vector3D Q(*qNeigh,*(qNeigh+1),*(qNeigh+2));
 	Array<double> r, theta, z;
 	double dr, d_theta, dz;
 	{
@@ -136,11 +134,8 @@ double RingVolumeFractionCalculator::operator() (const double* pCenter, const do
 
 double VolumeFractionCalculator::operator() (const double* pCenter, const double* qNeigh) const {
 
-	//const Vector3D P((double[3]){*pCenter,*(pCenter+1),*(pCenter+2)});
-	//const Vector3D Q((double[3]){*qNeigh,*(qNeigh+1),*(qNeigh+2)});
-	Vector3D P,Q;
-        P[0] = pCenter[0]; P[1] = pCenter[1]; P[2] = pCenter[2];
-        Q[0] = qNeigh[0]; Q[1] = qNeigh[1]; Q[2] = qNeigh[2];
+	const Vector3D P(*pCenter,*(pCenter+1),*(pCenter+2));
+	const Vector3D Q(*qNeigh,*(qNeigh+1),*(qNeigh+2));
 	Spec1D xSpec(nX,Q[0]-DX/2,DX);
 	Spec1D ySpec(nY,Q[1]-DY/2,DY);
 	Spec1D zSpec(nZ,Q[2]-DZ/2,DZ);
