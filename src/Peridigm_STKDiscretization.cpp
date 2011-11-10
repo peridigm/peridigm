a1cf66847bcf288dbab96e0bafdeeba4ad308c14
David Littlewood <djlittl@sandia.gov>
2011-11-10 17:25:30 +0000

    Bug fix in PeridigmNS::STKDiscretization.
    Reblessed regression test gold files.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@973 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 63d45e55d5bafd6f30ed2eabd2a590045d09d606

--- src/Peridigm_STKDiscretization.cpp
+++ src/Peridigm_STKDiscretization.cpp
@@ -542,7 +542,7 @@ double PeridigmNS::STKDiscretization::scalarTripleProduct(std::vector<double>& a
                                                           std::vector<double>& c) const
 {
   double tripleProduct = 
-    a[0]*(b[1] - c[2])*(b[2] - c[1]) - a[1]*(b[0] - c[2])*(b[2] - c[0]) + a[2]*(b[0] - c[1])*(b[1] - c[0]);
+    a[0]*(b[1]*c[2] - b[2]*c[1]) + a[1]*(b[2]*c[0] - b[0]*c[2]) + a[2]*(b[0]*c[1] - b[1]*c[0]);
 
   return tripleProduct;
 }
