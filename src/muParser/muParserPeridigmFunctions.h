276a3db7fa9a207050454c909f8cbd2ac4877fd3
Michael Parks <mlparks@sandia.gov>
2011-12-09 20:47:27 +0000

    Checkin of random number generator for peridigm. This should be an improvement over std::rand().
    Also, cleanup of muParser source with dos2unix.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1034 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: e313783f0bf0446fc552532bc37377e68c45d937

--- src/muParser/muParserPeridigmFunctions.h
+++ src/muParser/muParserPeridigmFunctions.h
@@ -2,11 +2,13 @@
 #define MU_PARSER_PERIDIGM_FUNCTIONS_H
 
 #include "muParser.h"
+#include "Peridigm_RandomNumber.hpp"
 
 namespace mu {
   static value_type Rnd(value_type magnitude) {
     value_type tmp;
-    tmp = (2.0*( (value_type)rand() / (value_type)RAND_MAX )) - 1.0;
+    value_type maxval = std::numeric_limits<value_type>::max();
+    tmp = (2.0*( (value_type)PeridigmNS::rand_num() / (value_type)maxval )) - 1.0;
     return ( magnitude*tmp );
   }
 } 
