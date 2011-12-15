e6dbfd2ca078fa613c4464e537ffae69049e273a
David Littlewood <djlittl@sandia.gov>
2011-12-15 20:41:31 +0000

    Groundwork for partial volume calculation.
    Created the function
    
    AbstractDiscretization::getExodusMeshNodePositions(int globalNodeID)
    
    and implemented it for STKDiscretization.  This function
    returns the positions of the nodes in the original hex/tet
    mesh read from file.  These data are needed for the calculation
    of partial volumes.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1036 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 05f3aa3f5ffd72b955578b4fd64f19e5552377b7

--- src/io/discretization/Peridigm_AbstractDiscretization.hpp
+++ src/io/discretization/Peridigm_AbstractDiscretization.hpp
@@ -118,6 +118,12 @@ namespace PeridigmNS {
     //! Get the locally-owned IDs for each node set
     virtual Teuchos::RCP< std::map< std::string, std::vector<int> > > getNodeSets() { return nodeSets; } ;
 
+    //! Get the node positions in the original Exodus hex/tet mesh.
+    virtual Teuchos::RCP< std::vector<double> > getExodusMeshNodePositions(int globalNodeID){
+      // The default implementation returns a null pointer.
+      return Teuchos::RCP< std::vector<double> >();
+    }
+
     //! Get the owned (non-overlap) map.
     static Epetra_BlockMap getOwnedMap(const Epetra_Comm& comm, const QUICKGRID::Data& gridData, int ndf);
 
