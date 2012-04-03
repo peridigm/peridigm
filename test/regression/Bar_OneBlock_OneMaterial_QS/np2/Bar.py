abd12703f24feeb54b82e4b8874ba164e1a121e6
David Littlewood <djlittl@sandia.gov>
2012-04-03 21:54:25 +0000

    Added simple quasi-statics bar test.
    This version has one material and a single block.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1121 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 626d4a5937c8d4723d5da9bc23b9119315246d50

---
+++ test/regression/Bar_OneBlock_OneMaterial_QS/np2/Bar.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import re
+from subprocess import Popen
+
+test_dir = "Bar_OneBlock_OneMaterial_QS/np2"
+base_name = "Bar"
+
+if __name__ == "__main__":
+
+    result = 0
+
+    # log file will be dumped if verbose option is given
+    verbose = False
+    if "-verbose" in sys.argv:
+        verbose = True
+
+    # change to the specified test directory
+    os.chdir(test_dir)
+
+    # open log file
+    log_file_name = base_name + ".log"
+    if os.path.exists(log_file_name):
+        os.remove(log_file_name)
+    logfile = open(log_file_name, 'w')
+
+    # remove old output files, if any
+    files_to_remove = base_name + ".e"
+    for file in os.listdir(os.getcwd()):
+      if file in files_to_remove:
+        os.remove(file)
+
+    # run Peridigm
+    command = ["mpiexec", "-np", "2", "../../../../src/Peridigm", "../"+base_name+".xml"]
+    p = Popen(command, stdout=logfile, stderr=logfile)
+    return_code = p.wait()
+    if return_code != 0:
+        result = return_code
+
+    # compare output files against gold files
+    command = ["../../../../scripts/epu", "-p", "2", base_name]
+    p = Popen(command, stdout=logfile, stderr=logfile)
+    return_code = p.wait()
+    if return_code != 0:
+        result = return_code
+    command = ["../../../../scripts/exodiff", \
+                   base_name+".e", \
+                   "../"+base_name+"_gold.e", \
+                   "-f", \
+                   "../"+base_name+".comp"]
+    p = Popen(command, stdout=logfile, stderr=logfile)
+    return_code = p.wait()
+    if return_code != 0:
+        result = return_code
+
+    logfile.close()
+
+    # dump the output if the user requested verbose
+    if verbose == True:
+        os.system("cat " + log_file_name)
+
+    sys.exit(result)
