eace275d615ea85b21e54bcc0bc1a9d886b8d8ac
Canio Hoffarth <choffar@sandia.gov>
2012-06-15 14:39:26 +0000

    This compute class calculates the kinetic energy, strain energy density and strain energy per element. The values can be specified as an ouput in the input deck.  The unit test has been created and added to the test project.
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1129 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 4ae02d5705ef81ef3feacd71c8ab502b75a3452f

---
+++ src/compute/Peridigm_Compute_Energy.hpp
@@ -0,0 +1,102 @@
+/*! \file Peridigm_Compute_Energy.hpp */
+
+//@HEADER
+// ************************************************************************
+//
+//                             Peridigm
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions?
+// David J. Littlewood   djlittl@sandia.gov
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+
+
+#ifdef COMPUTE_CLASS
+
+//ComputeClass(AngularMomentum,Compute_Energy,peridigm)
+
+// This is a hack to force this compute class to be created.
+// What we really want is a way, in the input deck, to request output data that is not related to a material model.
+ComputeClass(Displacement,Compute_Energy,peridigm)
+
+#else
+
+
+#ifndef PERIDIGM_COMPUTE_ENERGY_HPP
+#define PERIDIGM_COMPUTE_ENERGY_HPP
+
+#include "Peridigm_Compute.hpp"
+#include "Peridigm_DataManager.hpp"
+
+// Forward declaration
+namespace PeridigmNS {
+  class Peridigm;
+}
+
+namespace PeridigmNS {
+
+  //! Class for filling acceleration vector
+  class Compute_Energy : public PeridigmNS::Compute {
+
+  public:
+	
+  //! Standard constructor.
+  Compute_Energy( PeridigmNS::Peridigm *peridigm_ );
+
+  //! Destructor.
+  ~Compute_Energy();
+
+  //! Returns the fieldspecs computed by this class
+  std::vector<Field_NS::FieldSpec> getFieldSpecs() const;
+
+  //! Perform computation
+  int compute(const int numOwnedPoints,
+              const int* ownedIDs,
+              const int* neighborhoodList,
+              PeridigmNS::DataManager& dataManager) const;
+
+  private:
+
+  //! Parent pointer
+  PeridigmNS::Peridigm *peridigm;
+
+  };
+}
+
+#endif // PERIDIGM_COMPUTE_ENERGY_HPP
+#endif // COMPUTE_CLASS
