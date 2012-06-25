6f6a8469c0a29ed36c09dd5bb3ab0f67a249a804
Canio Hoffarth <choffar@sandia.gov>
2012-06-25 21:44:14 +0000

    Split the compute energy class into its components: kinetic energy, strain energy density and strain energy.
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1133 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: c30bce0595e86483a3a5d42a200eced88bd2c0a1

--- src/compute/Peridigm_Compute_Kinetic_Energy.cpp
+++ src/compute/Peridigm_Compute_Kinetic_Energy.cpp
@@ -1,4 +1,4 @@
-/*! \file Peridigm_Compute_Energy.cpp */
+/*! \file Peridigm_Compute_Kinetic_Energy.cpp */
 
 //@HEADER
 // ************************************************************************
@@ -47,24 +47,21 @@
 
 #include <vector>
 
-#include "Peridigm_Compute_Energy.hpp"
+#include "Peridigm_Compute_Kinetic_Energy.hpp"
 #include "../core/Peridigm.hpp"
 
 //! Standard constructor.
-PeridigmNS::Compute_Energy::Compute_Energy(PeridigmNS::Peridigm *peridigm_ ){peridigm = peridigm_;}
+PeridigmNS::Compute_Kinetic_Energy::Compute_Kinetic_Energy(PeridigmNS::Peridigm *peridigm_ ){peridigm = peridigm_;}
 
 //! Destructor.
-PeridigmNS::Compute_Energy::~Compute_Energy(){}
+PeridigmNS::Compute_Kinetic_Energy::~Compute_Kinetic_Energy(){}
 
 
 //! Returns the fieldspecs computed by this class
-std::vector<Field_NS::FieldSpec> PeridigmNS::Compute_Energy::getFieldSpecs() const 
+std::vector<Field_NS::FieldSpec> PeridigmNS::Compute_Kinetic_Energy::getFieldSpecs() const 
 {
   	std::vector<Field_NS::FieldSpec> myFieldSpecs;
   	myFieldSpecs.push_back(Field_NS::KINETIC_ENERGY);
-	myFieldSpecs.push_back(Field_NS::STRAIN_ENERGY);
-	myFieldSpecs.push_back(Field_NS::STRAIN_ENERGY_DENSITY);
-
 
   	return myFieldSpecs;
 }
@@ -72,32 +69,25 @@ std::vector<Field_NS::FieldSpec> PeridigmNS::Compute_Energy::getFieldSpecs() con
 
 
 //! Fill the energy vectors
-int PeridigmNS::Compute_Energy::compute( Teuchos::RCP< std::vector<PeridigmNS::Block> > blocks  ) const
+int PeridigmNS::Compute_Kinetic_Energy::compute( Teuchos::RCP< std::vector<PeridigmNS::Block> > blocks  ) const
 {
 	int retval;
 
 
-	Teuchos::RCP<Epetra_Vector> velocity, volume, force, ref, coord, w_volume, dilatation, numNeighbors, neighborID, kinetic_energy, strain_energy, strain_energy_density;
+	Teuchos::RCP<Epetra_Vector> velocity, volume, force, numNeighbors, neighborID, kinetic_energy;
 	std::vector<PeridigmNS::Block>::iterator blockIt;
 	for(blockIt = blocks->begin() ; blockIt != blocks->end() ; blockIt++){
 		Teuchos::RCP<PeridigmNS::DataManager> dataManager = blockIt->getDataManager();
 		Teuchos::RCP<PeridigmNS::NeighborhoodData> neighborhoodData = blockIt->getNeighborhoodData();
 		const int numOwnedPoints = neighborhoodData->NumOwnedPoints();
 		const int* ownedIDs = neighborhoodData->OwnedIDs();
-		const int* neighborhoodList = neighborhoodData->NeighborhoodList();
 
 		velocity              = dataManager->getData(Field_NS::VELOC3D, Field_ENUM::STEP_NP1);
 		volume                = dataManager->getData(Field_NS::VOLUME, Field_ENUM::STEP_NONE);
-		ref                   = dataManager->getData(Field_NS::COORD3D, Field_ENUM::STEP_NONE);
-		coord                 = dataManager->getData(Field_NS::CURCOORD3D, Field_ENUM::STEP_NP1);
-		w_volume              = dataManager->getData(Field_NS::WEIGHTED_VOLUME, Field_ENUM::STEP_NONE);
-		dilatation            = dataManager->getData(Field_NS::DILATATION, Field_ENUM::STEP_NP1);
 		kinetic_energy        = dataManager->getData(Field_NS::KINETIC_ENERGY, Field_ENUM::STEP_NP1);
-		strain_energy         = dataManager->getData(Field_NS::STRAIN_ENERGY, Field_ENUM::STEP_NP1);
-		strain_energy_density = dataManager->getData(Field_NS::STRAIN_ENERGY_DENSITY, Field_ENUM::STEP_NP1);
 	
 		// Sanity check
-		if (velocity->Map().NumMyElements() != volume->Map().NumMyElements() || velocity->Map().NumMyElements() != ref->Map().NumMyElements())
+		if (velocity->Map().NumMyElements() != volume->Map().NumMyElements())
 		{
 			retval = 1;
 			return(retval);
@@ -106,90 +96,40 @@ int PeridigmNS::Compute_Energy::compute( Teuchos::RCP< std::vector<PeridigmNS::B
 		// Collect values
 		double *volume_values = volume->Values();
 		double *velocity_values = velocity->Values();
-		double *ref_values = ref->Values();
-		double *coord_values = coord->Values();
-		double *w_volume_values = w_volume->Values();
-		double *dilatation_values = dilatation->Values();
 		double *kinetic_energy_values  = kinetic_energy->Values();
-		double *strain_energy_values  = strain_energy->Values();
-		double *strain_energy_density_values  = strain_energy_density->Values();
 	
 		// \todo Generalize this for multiple materials
 		// Get the material properties 
 		double density  = peridigm->getMaterialModels()->operator[](0)->Density();
-		double SM = peridigm->getMaterialModels()->operator[](0)->ShearModulus();
-		double BM = peridigm->getMaterialModels()->operator[](0)->BulkModulus();	
+		//double SM = peridigm->getMaterialModels()->operator[](0)->ShearModulus();
+		//double BM = peridigm->getMaterialModels()->operator[](0)->BulkModulus();	
 
-		// Initialize energy values
-		double KE,  SE;
-		KE = SE = 0.0;
-
-		// Initialize strain energy density
-		double W = 0.0; 
+		// Initialize kinetic energy value
+		double KE = 0.0;
 
 		// volume is a scalar and force a vector, so maps are different; must do multiplication on per-element basis
 		int numElements = numOwnedPoints;
 
-		double vol, vol2, w_vol;
+		double vol;
 
-		// Initialize local strain energy density
-		double We;
-
-		int neighborhoodListIndex = 0;
 		for (int i=0;i<numElements;i++) 
 		{
-			int numNeighbors = neighborhoodList[neighborhoodListIndex++];
 			int ID = ownedIDs[i];
-			We = 0.0;
 			vol = volume_values[ID];
-			w_vol = w_volume_values[ID];
 			double v1 = velocity_values[3*ID];
 			double v2 = velocity_values[3*ID+1];
 			double v3 = velocity_values[3*ID+2];
-			for (int j=0; j<numNeighbors; j++)
-			{
-				int neighborID = neighborhoodList[neighborhoodListIndex++];
-				TEUCHOS_TEST_FOR_EXCEPT_MSG(neighborID < 0, "Invalid neighbor list\n");
-				int Ne = neighborID;
-				vol2 = volume_values[Ne];
-				double psi1 = (ref_values[3*Ne] - ref_values[3*i]);
-				double psi2 = (ref_values[3*Ne+1] - ref_values[3*i+1]);
-				double psi3 = (ref_values[3*Ne+2] - ref_values[3*i+2]);
-				double eta1 = (coord_values[3*Ne] - coord_values[3*i]);
-				double eta2 = (coord_values[3*Ne+1] - coord_values[3*i+1]);
-				double eta3 = (coord_values[3*Ne+2] - coord_values[3*i+2]);
-
-				// Compute the reference position
-				double x = sqrt(psi1*psi1 + psi2*psi2 + psi3*psi3);
-
-				// Compute the deformed position
-				double y = sqrt(eta1*eta1 + eta2*eta2 + eta3*eta3);
-
-				// Compute the extension
-				double e = y-x;
-				// \todo Generalize for different influence functions
-				// Update the local strain energy density
-				We = We + (1.0)*(e - dilatation_values[ID]*x/3)*vol2;
-			}
-			// Update the strain energy density                
-			W = W + 0.5*BM*dilatation_values[ID]*dilatation_values[ID] + 0.5*(15.0*SM/w_vol)*We;		
-			strain_energy_density_values[i] = 0.5*BM*dilatation_values[ID]*dilatation_values[ID] + 0.5*(15.0*SM/w_vol)*We;		
-		
-			// Update the strain energy
-			strain_energy_values[i] = vol*strain_energy_density_values[i];
-			SE = SE + strain_energy_values[i];
+			
 			// Update the global kinetic energy
 			KE = KE + 0.5*vol*density*(v1*v1 + v2*v2 + v3*v3);
 			kinetic_energy_values[i] = 0.5*vol*density*(v1*v1 + v2*v2 + v3*v3);	
 		}
 
 		// Update info across processors
-		double localKE, localSE, globalKE, globalSE;
+		double localKE, globalKE;
 		localKE = KE;
-		localSE = SE;
 
 		peridigm->getEpetraComm()->SumAll(&localKE, &globalKE, 1);
-		peridigm->getEpetraComm()->SumAll(&localSE, &globalSE, 1);
 
 /*
         if (peridigm->getEpetraComm()->MyPID() == 1)
---
+++ src/compute/unit_test/utPeridigm_Compute_Kinetic_Energy.cpp
@@ -0,0 +1,231 @@
+/*! \file utPeridigm_Kinetic_Energy.cpp */
+
+//@HEADER
+// ************************************************************************
+//
+//                             Peridigm
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions?
+// David J. Littlewood   djlittl@sandia.gov
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+
+#include <Peridigm_AbstractDiscretization.hpp>
+#include "../Peridigm_Compute_Kinetic_Energy.hpp"
+#include <Peridigm_DataManager.hpp>
+#include <Peridigm_DiscretizationFactory.hpp>
+
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_ALTERNATIVE_INIT_API
+#include <boost/test/unit_test.hpp>
+#include <Epetra_ConfigDefs.h> // used to define HAVE_MPI
+#include <Epetra_Import.h>
+#include <Teuchos_ParameterList.hpp>
+#ifdef HAVE_MPI
+  #include <Epetra_MpiComm.h>
+#else
+  #include <Epetra_SerialComm.h>
+#endif
+#include <vector>
+#include "../../core/Peridigm.hpp"
+
+
+using namespace boost::unit_test;
+using namespace Teuchos;
+
+
+Teuchos::RCP<PeridigmNS::Peridigm> createFourPointModel()
+{
+        Teuchos::RCP<Epetra_Comm> comm;
+        #ifdef HAVE_MPI
+                comm = Teuchos::rcp(new Epetra_MpiComm(MPI_COMM_WORLD));
+        #else
+                comm = Teuchos::rcp(new Epetra_SerialComm);
+        #endif
+
+
+	// set up parameter lists
+ 	// these data would normally be read from an input xml file
+        RCP<Teuchos::ParameterList> peridigmParams = rcp(new Teuchos::ParameterList());
+
+        // problem parameters
+        ParameterList& problemParams = peridigmParams->sublist("Problem");
+        problemParams.set("Verbose", false);
+
+        // material parameters
+        ParameterList& materialParams = problemParams.sublist("Material");
+        ParameterList& linearElasticMaterialParams = materialParams.sublist("Linear Elastic");
+        linearElasticMaterialParams.set("Density", 7800.0);
+        linearElasticMaterialParams.set("Bulk Modulus", 130.0e9);
+        linearElasticMaterialParams.set("Shear Modulus", 78.0e9);
+
+        // Set up discretization parameterlist
+        ParameterList& discretizationParams = problemParams.sublist("Discretization");
+        discretizationParams.set("Type", "PdQuickGrid");
+        discretizationParams.set("Horizon", 5.0);
+        // pdQuickGrid tensor product mesh generator parameters
+        ParameterList& pdQuickGridParams = discretizationParams.sublist("TensorProduct3DMeshGenerator");
+        pdQuickGridParams.set("Type", "PdQuickGrid");
+        pdQuickGridParams.set("X Origin",  0.0);
+        pdQuickGridParams.set("Y Origin",  0.0);
+        pdQuickGridParams.set("Z Origin",  0.0);
+        pdQuickGridParams.set("X Length",  6.0);
+        pdQuickGridParams.set("Y Length",  1.0);
+        pdQuickGridParams.set("Z Length",  1.0);
+        pdQuickGridParams.set("Number Points X", 4);
+        pdQuickGridParams.set("Number Points Y", 1);
+        pdQuickGridParams.set("Number Points Z", 1);
+
+        // output parameters (to force instantiation of energy compute classes and data storage in DataManager)
+       	ParameterList& outputParams = peridigmParams->sublist("Output");
+        ParameterList& materialOutputFields = outputParams.sublist("Material Output Fields");
+        ParameterList& linearElasticMaterialFields = materialOutputFields.sublist("Linear Elastic");
+        linearElasticMaterialFields.set("Kinetic_Energy", true);
+
+	// create the Peridigm object
+	RCP<PeridigmNS::Peridigm> peridigm = Teuchos::rcp(new PeridigmNS::Peridigm(comm, peridigmParams));
+
+	return peridigm;
+}
+
+
+void FourPointTest() 
+{
+	Teuchos::RCP<PeridigmNS::Peridigm> peridigm = createFourPointModel();
+
+        // Get the neighborhood data
+        PeridigmNS::NeighborhoodData neighborhoodData = (*peridigm->getGlobalNeighborhoodData());
+  	// Get the data manager
+        Teuchos::RCP<PeridigmNS::DataManager> dataManager = (*peridigm->getDataManagers())[0];
+  	// Access the data we need
+        Teuchos::RCP<Epetra_Vector> velocity, volume, ref, coords, dilatation, kinetic_energy;
+        velocity              = dataManager->getData(Field_NS::VELOC3D, Field_ENUM::STEP_NP1);
+	volume                = dataManager->getData(Field_NS::VOLUME, Field_ENUM::STEP_NONE);
+	ref                   = dataManager->getData(Field_NS::COORD3D, Field_ENUM::STEP_NONE);
+	coords                = dataManager->getData(Field_NS::CURCOORD3D, Field_ENUM::STEP_NP1);
+	dilatation            = dataManager->getData(Field_NS::DILATATION, Field_ENUM::STEP_NP1);
+	kinetic_energy        = dataManager->getData(Field_NS::KINETIC_ENERGY, Field_ENUM::STEP_NP1);
+        // Get the neighborhood structure
+	const int numOwnedPoints = (neighborhoodData.NumOwnedPoints());
+
+	// Manufacture velocity data
+	double *velocity_values  = velocity->Values();
+	double *ref_values = ref->Values();
+	double *coords_values = coords->Values();
+	double *dilatation_values = dilatation->Values();
+	int *myGIDs = velocity->Map().MyGlobalElements();
+        int numElements = numOwnedPoints;
+	int numTotalElements = volume->Map().NumMyElements();
+	for (int i=0;i<numTotalElements;i++) 
+	{
+		int ID = myGIDs[i];
+    		velocity_values[3*i] = 3.0*ID;
+    		velocity_values[3*i+1] = (3.0*ID)+1.0;
+    		velocity_values[3*i+2] = (3.0*ID)+2.0;
+	}
+
+  	// Create Compute_Energy object
+  	Teuchos::RCP<PeridigmNS::Compute_Kinetic_Energy> computeKineticEnergy = Teuchos::rcp(new PeridigmNS::Compute_Kinetic_Energy(&(*peridigm)));
+
+	// Get the blocks
+	Teuchos::RCP< std::vector<PeridigmNS::Block> > blocks = peridigm->getBlocks();
+
+  	// Call the compute class
+	int retval = computeKineticEnergy->compute( blocks );
+  	BOOST_CHECK_EQUAL( retval, 0 );
+
+	// \todo Generalize this for multiple materials
+	double density = peridigm->getMaterialModels()->operator[](0)->Density();
+	
+  	// Now check that volumes and energy is correct
+  	double *volume_values = volume->Values();
+  	double *kinetic_energy_values  = kinetic_energy->Values();
+  	for (int i=0;i<numElements;i++)
+    		BOOST_CHECK_CLOSE(volume_values[i], 1.5, 1.0e-15);
+  	for (int i=0;i<numElements;i++) 
+	{
+		int ID = myGIDs[i];
+                double mass = density*volume_values[ID];
+    		BOOST_CHECK_CLOSE(kinetic_energy_values[i],  0.5*mass*(pow((3.0*ID),2)+pow(((3.0*ID)+1.0),2)+pow(((3.0*ID)+2.0),2)), 1.0e-15);
+	}
+}
+
+
+bool init_unit_test_suite() 
+{
+  	// Add a suite for each processor in the test
+ 	bool success = true;
+
+	test_suite* proc = BOOST_TEST_SUITE("utPeridigm_Compute_Kinetic_Energy");
+  	proc->add(BOOST_TEST_CASE(&FourPointTest));
+  	framework::master_test_suite().add(proc);
+
+  	return success;
+}
+
+bool init_unit_test() 
+{
+  	return init_unit_test_suite();
+}
+
+int main (int argc, char* argv[]) 
+{
+  	int numProcs = 1;
+	#ifdef HAVE_MPI
+  		MPI_Init(&argc,&argv);
+  		MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+	#endif
+
+  	int returnCode = -1;
+  
+  	if(numProcs >= 1 && numProcs <= 4)
+	{
+    		returnCode = unit_test_main(init_unit_test, argc, argv);
+	}
+	else
+	{
+		std::cerr << "Unit test runtime ERROR: utPeridigm_Compute_Kinetic_Energy only makes sense on 1 to 4 processors." << std::endl;
+	}
+  
+	#ifdef HAVE_MPI
+  		MPI_Finalize();
+	#endif
+  
+  	return returnCode;
+}
