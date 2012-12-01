2682322258eac1cdaf11c36181bee8a057aae4b2
David Littlewood <djlittl@sandia.gov>
2012-12-01 00:09:53 +0000

    Forgot to "svn add" the files...
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1269 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 0ef908f39e16be1aa3834d22d3b9666112ef80d8

---
+++ src/compute/Peridigm_Compute_Block_Data.cpp
@@ -0,0 +1,226 @@
+/*! \file Peridigm_Compute_Block_Data.cpp */
+
+//@HEADER
+// ************************************************************************
+//
+//                             Peridigm
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions?
+// David J. Littlewood   djlittl@sandia.gov
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+
+#include <vector>
+
+#include "Peridigm_Compute_Block_Data.hpp"
+#include "Peridigm_Field.hpp"
+
+using namespace std;
+
+//! Standard constructor.
+PeridigmNS::Compute_Block_Data::Compute_Block_Data(Teuchos::RCP<const Teuchos::ParameterList> params,
+                                                   Teuchos::RCP<const Epetra_Comm> epetraComm_)
+  : Compute(params, epetraComm_), m_blockId(-1), m_calculationType(UNDEFINED_CALCULATION),
+    m_variableFieldId(-1), m_outputFieldId(-1), m_outputXFieldId(-1), m_outputYFieldId(-1),
+    m_outputZFieldId(-1)
+{
+  m_blockName = params->get<string>("Block");
+  m_variable = params->get<string>("Variable");
+  m_outputLabel = params->get<string>("Output Label");
+
+  string calculationType = params->get<string>("Calculation Type");
+  if(calculationType == "Minimum"){
+    m_calculationType = MINIMUM;
+  }
+  else if(calculationType == "Maximum"){
+    m_calculationType = MAXIMUM;
+  }
+  else if(calculationType == "Sum"){
+    m_calculationType = SUM;
+  }
+  else{
+    TEUCHOS_TEST_FOR_EXCEPT_MSG(true, 
+     "**** Error:  invalid \"Calculation Type\" in Block_Data compute class, must be \"Minimum\", \"Maximum\", or \"Sum\".\n");
+  }
+
+  FieldManager& fieldManager = FieldManager::self();
+  m_variableFieldId = fieldManager.getFieldId(m_variable);
+  m_fieldIds.push_back(m_variableFieldId);
+
+  PeridigmField::Length length = fieldManager.getFieldSpec(m_variableFieldId).getLength();
+  if(length == PeridigmField::SCALAR)
+    m_variableLength = 1;
+  else if(length == PeridigmField::VECTOR)
+    m_variableLength = 3;
+  else
+    TEUCHOS_TEST_FOR_EXCEPT_MSG(true, "**** Error:  Block_Data compute class can be called only for SCALAR or VECTOR data.\n");
+
+  if(m_variableLength == 1){
+    m_outputFieldId = fieldManager.getFieldId(PeridigmField::GLOBAL, PeridigmField::SCALAR, PeridigmField::CONSTANT, m_outputLabel);
+    m_fieldIds.push_back(m_outputFieldId);
+  }
+  else if(m_variableLength == 3){
+    m_outputXFieldId = fieldManager.getFieldId(PeridigmField::GLOBAL, PeridigmField::SCALAR, PeridigmField::CONSTANT, m_outputLabel+"_X");
+    m_outputYFieldId = fieldManager.getFieldId(PeridigmField::GLOBAL, PeridigmField::SCALAR, PeridigmField::CONSTANT, m_outputLabel+"_Y");
+    m_outputZFieldId = fieldManager.getFieldId(PeridigmField::GLOBAL, PeridigmField::SCALAR, PeridigmField::CONSTANT, m_outputLabel+"_Z");
+    m_fieldIds.push_back(m_outputXFieldId);
+    m_fieldIds.push_back(m_outputYFieldId);
+    m_fieldIds.push_back(m_outputZFieldId);
+  }
+
+  PeridigmField::Temporal temporal = fieldManager.getFieldSpec(m_variableFieldId).getTemporal();
+  m_variableIsStated = false;
+  if(temporal == PeridigmField::TWO_STEP)
+    m_variableIsStated = true;
+}
+
+void PeridigmNS::Compute_Block_Data::initialize( Teuchos::RCP< std::vector<PeridigmNS::Block> > blocks ) {
+
+  m_blockId = -1;
+  for(std::vector<Block>::iterator blockIt = blocks->begin() ; blockIt != blocks->end() ; blockIt++){
+    if(blockIt->getName() == m_blockName)
+      m_blockId = blockIt->getID();
+  }
+
+  if(m_blockId == -1){
+    string msg = "**** Error:  Block_Data compute class failed to find block: " + m_blockName + "\n";
+    TEUCHOS_TEST_FOR_EXCEPT_MSG(true, msg);
+  }
+
+}
+
+int PeridigmNS::Compute_Block_Data::compute( Teuchos::RCP< std::vector<PeridigmNS::Block> > blocks ) const {
+
+  PeridigmField::Step step = PeridigmField::STEP_NONE;
+  if(m_variableIsStated)
+    step = PeridigmField::STEP_NP1;
+  
+  vector<double> localData(3), globalData(3);
+
+  if(m_calculationType == MINIMUM){
+    for(int i=0 ; i<3 ; ++i)
+      localData[i] = DBL_MAX;
+  }
+  else if(m_calculationType == MAXIMUM){
+    for(int i=0 ; i<3 ; ++i)
+      localData[i] = DBL_MIN;
+  }
+  else if(m_calculationType == SUM){
+    for(int i=0 ; i<3 ; ++i)
+      localData[i] = 0.0;
+  }
+
+  for(std::vector<Block>::iterator blockIt = blocks->begin() ; blockIt != blocks->end() ; blockIt++){
+    if(blockIt->getID() == m_blockId){
+      double *data;
+      blockIt->getData(m_variableFieldId, step)->ExtractView(&data);
+      int numOwnedPoints = blockIt->getNeighborhoodData()->NumOwnedPoints();
+      if(m_calculationType == MINIMUM){
+        for(int i=0 ; i<numOwnedPoints ; ++i){
+          if(m_variableLength == 1){
+            if(data[i] < localData[0])
+              localData[0] = data[i];
+          }
+          else if(m_variableLength == 3){
+            if(data[3*i] < localData[0])
+              localData[0] = data[3*i];
+            if(data[3*i] < localData[0])
+              localData[1] = data[3*i+1];
+            if(data[3*i] < localData[0])
+              localData[2] = data[3*i+2];
+          }
+        }
+      }
+      else if(m_calculationType == MAXIMUM){
+        for(int i=0 ; i<numOwnedPoints ; ++i){
+          if(m_variableLength == 1){
+            if(data[i] > localData[0])
+              localData[0] = data[i];
+          }
+          else if(m_variableLength == 3){
+            if(data[3*i] > localData[0])
+              localData[0] = data[3*i];
+            if(data[3*i] > localData[0])
+              localData[1] = data[3*i+1];
+            if(data[3*i] > localData[0])
+              localData[2] = data[3*i+2];
+          }
+        }
+      }
+      else if(m_calculationType == SUM){
+        for(int i=0 ; i<numOwnedPoints ; ++i){
+          if(m_variableLength == 1){
+            localData[0] += data[i];
+          }
+          else if(m_variableLength == 3){
+            localData[0] += data[3*i];
+            localData[1] += data[3*i+1];
+            localData[2] += data[3*i+2];
+          }
+        }
+      }
+    }
+  }
+
+  if(m_variableLength == 1){
+    if(m_calculationType == MINIMUM)
+      epetraComm()->MinAll(&localData[0], &globalData[0], 1);
+    else if(m_calculationType == MAXIMUM)
+      epetraComm()->MaxAll(&localData[0], &globalData[0], 1);
+    else if(m_calculationType == SUM)
+      epetraComm()->SumAll(&localData[0], &globalData[0], 1);
+    double& outputData = blocks->begin()->getGlobalData(m_outputFieldId);
+    outputData = globalData[0];
+  }
+  else if(m_variableLength == 3){
+    if(m_calculationType == MINIMUM)
+      epetraComm()->MinAll(&localData[0], &globalData[0], 3);
+    else if(m_calculationType == MAXIMUM)
+      epetraComm()->MaxAll(&localData[0], &globalData[0], 3);
+    else if(m_calculationType == SUM)
+      epetraComm()->SumAll(&localData[0], &globalData[0], 3);
+    double& outputDataX = blocks->begin()->getGlobalData(m_outputXFieldId);
+    outputDataX = globalData[0];
+    double& outputDataY = blocks->begin()->getGlobalData(m_outputYFieldId);
+    outputDataY = globalData[1];
+    double& outputDataZ = blocks->begin()->getGlobalData(m_outputZFieldId);
+    outputDataZ = globalData[2];
+  }
+
+  return 0;
+}
---
+++ src/compute/Peridigm_Compute_Block_Data.hpp
@@ -0,0 +1,112 @@
+/*! \file Peridigm_Compute_Block_Data.hpp */
+
+//@HEADER
+// ************************************************************************
+//
+//                             Peridigm
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions?
+// David J. Littlewood   djlittl@sandia.gov
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+
+#ifdef COMPUTE_CLASS
+
+ComputeClass(Block_Data,Compute_Block_Data)
+
+#else
+
+#ifndef PERIDIGM_COMPUTE_BLOCK_DATA_HPP
+#define PERIDIGM_COMPUTE_BLOCK_DATA_HPP
+
+#include "Peridigm_Compute.hpp"
+
+namespace PeridigmNS {
+
+  //! Class for tracking the data at a specified point in space
+  class Compute_Block_Data : public PeridigmNS::Compute {
+
+  public:
+	
+    //! Standard constructor.
+    Compute_Block_Data( Teuchos::RCP<const Teuchos::ParameterList> params,
+                        Teuchos::RCP<const Epetra_Comm> epetraComm_ );
+
+    //! Destructor.
+    ~Compute_Block_Data() {}
+
+    //! Returns a vector of field IDs corresponding to the variables associated with the compute class.
+    virtual std::vector<int> FieldIds() const { return m_fieldIds; }
+
+    //! Initialize the compute class
+    virtual void initialize( Teuchos::RCP< std::vector<PeridigmNS::Block> > blocks );
+
+    //! Perform computation
+    virtual int compute( Teuchos::RCP< std::vector<PeridigmNS::Block> > blocks ) const;
+
+  private:
+
+    //! Name of variable to be tracked
+    std::string m_variable;
+    int m_variableLength;
+    bool m_variableIsStated;
+    string m_blockName;
+    int m_blockId;
+
+    enum CALCULATION_TYPE {
+      UNDEFINED_CALCULATION = 0,
+      MINIMUM,
+      MAXIMUM,
+      SUM
+    } m_calculationType ;
+
+    //! Label for output variable
+    std::string m_outputLabel;
+
+    //! Field ids for all relevant data
+    std::vector<int> m_fieldIds;
+    int m_variableFieldId;
+    int m_outputFieldId;
+    int m_outputXFieldId;
+    int m_outputYFieldId;
+    int m_outputZFieldId;
+  };
+}
+
+#endif // PERIDIGM_COMPUTE_BLOCK_DATA_HPP
+#endif // COMPUTE_CLASS
