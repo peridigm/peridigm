e3efe7a9ad93ea55ef8693c2f57d4e9ffe330f2d
David Littlewood <djlittl@sandia.gov>
2013-01-03 23:33:00 +0000

    Added functionality for specifying an influence function.
    The influnce function is specified via a string
    passed in through the Discretization section of
    the input deck, as follows:
    
    <ParameterList name="Discretization">
      ...
      <Parameter name="Influence Function" type="string" value="One" />
      ...
    </ParameterList>
    
    Currently, the only valid option is One, which just sets
    the influence function to 1.0.  The game plan is to add a
    small set of built-in options, and then later to enable
    arbitrary user-defined influence functions.
    
    From the material modeler's point of view, the value of
    the influence function for a given point and a given
    neighbor is obtained by calling the function:
    
    double scalarInfluenceFunction(double zeta, double horizon)
    
    which is in material_utilities.cxx.  This function
    is intended to be called from within the c-style material
    evaluation functions.
    
    It would also be possible for a material model to compute
    influence function values only once, at initialization, and
    store them as bond variables.  This decision is left to the
    discretion of the material model developer.  All current
    models call scalarInfluenceFunction() directly within each
    internal force evaluation.
    
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1307 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: ef225e4a4511264e80864f14ba09a3eb39ef38f3

---
+++ src/core/Peridigm_InfluenceFunction.hpp
@@ -0,0 +1,109 @@
+/*! \file Peridigm_InfluenceFunction.hpp */
+
+//@HEADER
+// ************************************************************************
+//
+//                             Peridigm
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions?
+// David J. Littlewood   djlittl@sandia.gov
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+
+#ifndef PERIDIGM_INFLUENCEFUNCTION_HPP
+#define PERIDIGM_INFLUENCEFUNCTION_HPP
+
+#include <Teuchos_Assert.hpp>
+#include <string>
+
+namespace PeridigmNS{
+
+namespace PeridigmInfluenceFunction{
+
+static double one(double zeta, double horizon){ return 1.0; }
+
+}
+
+class InfluenceFunction {
+
+public:
+
+  //! Type definition for the function pointer to an influence function
+  typedef double (*functionPointer)(double, double);
+
+  //! Singleton.
+  static InfluenceFunction & self() {
+    static InfluenceFunction influenceFunction;
+    return influenceFunction;
+  }
+
+  void setInfluenceFunction(std::string influenceFunctionString) {
+    if(influenceFunctionString == "One"){
+      m_influenceFunction = &PeridigmInfluenceFunction::one;
+    }
+    else{
+      TEUCHOS_TEST_FOR_EXCEPT_MSG(true, "**** Error:  InfluenceFunction::setInfluenceFunction(), invalid influence function, must be \"One\"\n");
+    }
+  }
+
+  functionPointer getInfluenceFunction() {
+    TEUCHOS_TEST_FOR_EXCEPT_MSG(m_influenceFunction == NULL,
+                                "**** Error:  InfluenceFunction::getInfluenceFunction() called prior to calling InfluenceFunction::setInfluenceFunction().\n")
+    return m_influenceFunction;
+  }
+
+private:
+
+  //! Constructor, private to prevent use (singleton class).
+  InfluenceFunction() : m_influenceFunction(NULL) {
+    setInfluenceFunction("One");
+  }
+
+  //! Private and unimplemented to prevent use
+  InfluenceFunction( const InfluenceFunction & );
+
+  //! Private and unimplemented to prevent use
+  InfluenceFunction & operator= ( const InfluenceFunction & );
+
+  //! Function pointer to the influence function with the signature:  double function(double zeta, double horizon).
+  functionPointer m_influenceFunction;
+};
+
+}
+
+#endif // PERIDIGM_INFLUENCEFUNCTION_HPP
