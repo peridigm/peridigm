3f9bc01e2765ebf9b257e1ed92df4890a8a94316
David Littlewood <djlittl@sandia.gov>
2013-01-10 18:27:56 +0000

    Added random and dumbbell meshes to CreateTestMeshes.py.
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1318 d9297710-12f9-482b-b11f-fb677dc88bbb

--- src/io/unit_test/input_files/CreateTestMeshes.py
+++ src/io/unit_test/input_files/CreateTestMeshes.py
@@ -1,5 +1,29 @@
 #! /usr/bin/env python
 
+from math import sqrt
+
+def crossProduct(a, b):
+    
+    return ( (a[1]*b[2]-a[2]*b[1]), (a[2]*b[0]-a[0]*b[2]), (a[0]*b[1]-a[1]*b[0]) )
+
+def norm(a):
+
+    return sqrt( a[0]*a[0] + a[1]*a[1] + a[2]*a[2] )
+
+class poorMansDeviceIndependentRandomNumber:
+
+    def __init__(self):
+        # Random number seeds
+        self.m_w = 1234
+        self.m_z = 1004321
+ 
+    def rand(self):
+        # 32-bit result
+        self.m_z = 36969 * (self.m_z & 65535) + (self.m_z >> 16)
+        self.m_w = 18000 * (self.m_w & 65535) + (self.m_w >> 16)
+        val = (self.m_z << 16) + self.m_w
+        return val
+
 def createEquallySpacedCubeMesh1000():
     
     xLow = 0.5
@@ -78,6 +102,70 @@ def createEquallySpacedCubeMesh27000():
 
     return mesh
 
+def createDumbbellMesh():
+    
+    xLow = 0.0
+    xHigh = 10.0
+    xNum = 60
+    yLow = 0.0
+    yHigh = 10.0
+    yNum = 80
+    zLow = 0.0
+    zHigh = 10.0
+    zNum = 100
+    volume = 10.0*((float(xNum)+1.0)/float(xNum))/float(xNum) * \
+        10.0*((float(yNum)+1.0)/float(yNum))/float(yNum) * \
+        10.0*((float(zNum)+1.0)/float(zNum))/float(zNum) 
+    material = 1
+
+    diagonalThreshold = 0.2
+    d1 = (xLow, yLow, zLow)
+    d2 = (xHigh, yHigh, zHigh)
+    
+    radius1 = 1.5
+    center1 = (xHigh - radius1, yHigh - radius1, zHigh - radius1)
+
+    radius2 = 0.75
+    center2 = (xLow + radius2, yLow + radius2, zLow + radius2)
+
+    mesh = []
+
+    temp = norm( (d2[0]-d1[0], d2[1]-d1[1], d2[2]-d1[2]) )
+
+    for i in range(xNum):
+        for j in range(yNum):
+            for k in range(zNum):
+                x = xLow + i*(xHigh - xLow)/(xNum - 1)
+                y = yLow + j*(yHigh - yLow)/(yNum - 1)
+                z = zLow + k*(zHigh - zLow)/(zNum - 1)
+
+                distanceFromDiagonal = norm( crossProduct( (x-d1[0], y-d1[1], z-d1[2]), (x-d2[0], y-d2[1], z-d2[2]) ) ) / temp
+                distanceFromCenter1 = norm( (x-center1[0], y-center1[1], z-center1[2]) )
+                distanceFromCenter2 = norm( (x-center2[0], y-center2[1], z-center2[2]) )
+
+                if distanceFromDiagonal < diagonalThreshold or distanceFromCenter1 < radius1 or distanceFromCenter2 < radius2:
+                    mesh.append( (x, y, z, material, volume) )
+
+    return mesh
+
+def createRandomMesh():
+    
+    low = 0.0
+    high = 10.0
+    mesh = []
+    numPts = 8000
+    volume = 0.5
+    material = 1
+    r = poorMansDeviceIndependentRandomNumber()
+
+    for i in range(numPts):
+        x = r.rand()
+        y = r.rand()
+        z = r.rand()
+        mesh.append( (x, y, z, material, volume) )
+
+    return mesh
+
 def writeTestMeshes():
 
     mesh = createEquallySpacedCubeMesh1000()
@@ -100,6 +188,24 @@ def writeTestMeshes():
 
     mesh = createEquallySpacedCubeMesh27000()
     fname = "cube_27000.txt"
+    fout = open(fname, 'w')
+    fout.write("# x y z block_id volume\n");
+    for pt in mesh:
+        fout.write(str(pt[0]) + " " + str(pt[1]) + " " + str(pt[2]) + " " + str(pt[3]) + " " + str(pt[4]) + "\n")
+    fout.close()
+    print "Wrote", fname, "\n"
+
+    mesh = createDumbbellMesh()
+    fname = "dumbbell.txt"
+    fout = open(fname, 'w')
+    fout.write("# x y z block_id volume\n");
+    for pt in mesh:
+        fout.write(str(pt[0]) + " " + str(pt[1]) + " " + str(pt[2]) + " " + str(pt[3]) + " " + str(pt[4]) + "\n")
+    fout.close()
+    print "Wrote", fname, "\n"
+
+    mesh = createRandomMesh()
+    fname = "random.txt"
     fout = open(fname, 'w')
     fout.write("# x y z block_id volume\n");
     for pt in mesh:
