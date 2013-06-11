0fb5974c168a8716363656efcee62e48906e2c8a
John Foster <johntfosterjr@gmail.com>
2013-06-11 17:32:31 +0000

    xmlripper.py should now produce correct input decks in the new format from old xml based input decks.
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1431 d9297710-12f9-482b-b11f-fb677dc88bbb

--- scripts/xmlripper.py
+++ scripts/xmlripper.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python
 
-''' strip information from an xml file and import in to a txt file name, value, type seperated by comma's
+''' parse Teuchos compatible xml based ParameterList to txt based ParameterList: standard xml files accepted also No Error Handling
 '''
 import xml.etree.ElementTree as et, sys
 
+
+def sTn(val):
+	if val.isdigit():
+		return float(val)
+	else:
+		return val
+
 def main(flname):
-	tree = et.parse(flname + '.xml')
+	tree = et.parse(flname)
 	root = tree.getroot()
-	fl = open(flname+'.txt','w+')
-	for child in root.iter():
-		if(child.get('name')):
-			if(child.get('value')):
-				fl.write('  ' + child.attrib['name'] + ' "' + child.attrib['value'] + '"\n')
-			else:
-				fl.write(child.attrib['name'] + '\n')
-	fl.close()
+	fl = open(flname.replace('.xml', '.txt'),'w+')
+	dataVec = []
+	i = 1
+	for param in root.findall("./"):
+#		print param.attrib
+		if param.get('value'):
+			if param.attrib['type'] == 'string' or param.attrib['type'] == 'bool':
+				fl.write(param.attrib['name'] + ' "' + param.attrib['value'] + '"\n')
+			elif param.attrib['type'] == 'int' or param.attrib['type'] == 'double':
+				fl.write(param.attrib['name'] + ' ' + param.attrib['value'] + '\n')
+		if not param.get('value'):
+			fl.write(param.attrib['name'] + '\n')
+			j = 1
+			for subparam in root.findall("./ParameterList[%s]/"%i):
+#				print subparam.attrib
+				if subparam.get('value'):
+					if subparam.attrib['type'] == 'string' or subparam.attrib['type'] == 'bool':
+						fl.write('  ' + subparam.attrib['name'] + ' "' + subparam.attrib['value'] + '"\n')
+					elif subparam.attrib['type'] == 'int' or subparam.attrib['type'] == 'double':
+						fl.write('  ' + subparam.attrib['name'] + ' ' + subparam.attrib['value'] + '\n')
+				if not subparam.get('value'):
+					fl.write('  ' + subparam.attrib['name'] + '\n')
+					for sub_subparam in root.findall("./ParameterList[%s]/ParameterList[%s]/" %(i, j)):
+#						print sub_subparam.attrib
+						if sub_subparam.get('value'):
+							if sub_subparam.attrib['type'] == 'string' or sub_subparam.attrib['type'] == 'bool':
+								fl.write('  ' + '  ' + sub_subparam.attrib['name'] + ' "' + sub_subparam.attrib['value'] + '"\n')
+							elif sub_subparam.attrib['type'] == 'int' or sub_subparam.attrib['type'] == 'double':
+								fl.write('  ' + '  ' + sub_subparam.attrib['name'] + ' ' + sub_subparam.attrib['value'] + '\n')
+					j = j+1
+			i = i+1
 def __init__():
 	flname = sys.argv[-1]
 	print(flname)
