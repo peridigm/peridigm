884e5d512b6d20b2ff557467c97c101ae464ae96
David Littlewood <djlittl@sandia.gov>
2013-07-15 16:58:56 +0000

    Created compute class to output the horizon for each element.
    "Horizon" can now be requested as an output variable.
    
    
    
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1453 d9297710-12f9-482b-b11f-fb677dc88bbb

---
+++ src/compute/Peridigm_Compute_Horizon.cpp
@@ -0,0 +1,77 @@
+/*! \file Peridigm_Compute_Horizon.cpp */
+
+//@HEADER
+// ************************************************************************
+//
+//                             Peridigm
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions?
+// David J. Littlewood   djlittl@sandia.gov
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+
+#include <vector>
+
+#include "Peridigm_Compute_Horizon.hpp"
+#include "Peridigm_Field.hpp"
+
+PeridigmNS::Compute_Horizon::Compute_Horizon(Teuchos::RCP<const Teuchos::ParameterList> params,
+                                             Teuchos::RCP<const Epetra_Comm> epetraComm_,
+                                             Teuchos::RCP<const Teuchos::ParameterList> computeClassGlobalData_)
+  : Compute(params, epetraComm_, computeClassGlobalData_), m_horizonFieldId(-1)
+{
+  FieldManager& fieldManager = FieldManager::self();
+  m_horizonFieldId = fieldManager.getFieldId(PeridigmField::ELEMENT, PeridigmField::SCALAR, PeridigmField::CONSTANT, "Horizon");
+  m_fieldIds.push_back(m_horizonFieldId);
+}
+
+PeridigmNS::Compute_Horizon::~Compute_Horizon(){}
+
+void PeridigmNS::Compute_Horizon::initialize( Teuchos::RCP< std::vector<PeridigmNS::Block> > blocks ) {
+
+  for(std::vector<PeridigmNS::Block>::iterator blockIt = blocks->begin() ; blockIt != blocks->end() ; blockIt++){
+    double horizon = blockIt->getHorizon();
+    Teuchos::RCP<Epetra_Vector> horizons = blockIt->getDataManager()->getData(m_horizonFieldId, PeridigmField::STEP_NONE);
+    for(unsigned int i=0 ; i<horizons->MyLength() ; ++i)
+      (*horizons)[i] = horizon;
+  }
+}
+
+int PeridigmNS::Compute_Horizon::compute( Teuchos::RCP< std::vector<PeridigmNS::Block> > blocks ) const {
+  return 0;
+}
--- src/compute/compute_includes.hpp
+++ src/compute/compute_includes.hpp
@@ -12,6 +12,7 @@
 #include "./compute/Peridigm_Compute_Contact_Force.hpp"
 #include "./compute/Peridigm_Compute_Force.hpp"
 #include "./compute/Peridigm_Compute_Radius.hpp"
+#include "./compute/Peridigm_Compute_Horizon.hpp"
 #include "./compute/Peridigm_Compute_Number_Of_Neighbors.hpp"
 #include "./compute/Peridigm_Compute_Neighborhood_Volume.hpp"
 #include "./compute/Peridigm_Compute_Nearest_Point_Data.hpp"
