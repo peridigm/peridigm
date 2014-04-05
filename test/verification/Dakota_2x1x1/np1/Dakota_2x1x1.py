39e5327c54a85888223d7d5a291611d30a71932b
David Littlewood <djlittl@sandia.gov>
2014-04-05 05:59:20 +0000

    Refactored scripts for Dakota test.
    git-svn-id: svn+ssh://software.sandia.gov/svn/private/peridigm/trunk@1672 d9297710-12f9-482b-b11f-fb677dc88bbb
    
    Former-commit-id: 0582e054417e5a11a913da4678995bfde7a9bd7a

--- test/verification/Dakota_2x1x1/np1/Dakota_2x1x1.py
+++ test/verification/Dakota_2x1x1/np1/Dakota_2x1x1.py
@@ -3,12 +3,19 @@
 import sys
 import os
 import re
+import string
 from subprocess import Popen
 
 test_dir = "Dakota_2x1x1/np1"
 base_name = "Dakota_2x1x1"
 
 if __name__ == "__main__":
+
+    # This test used Dakota to calibrate a constituive model
+    # via optimization.  The know correct value for the bulk
+    # moduls is 130.0 GPA
+    MODULUS_TRUTH_VALUE = 1.3000000000e+11
+    REL_ERROR_TOLERANCE = 1.0e-8
 
     result = 0
 
@@ -26,10 +33,10 @@ if __name__ == "__main__":
         os.remove(log_file_name)
     logfile = open(log_file_name, 'w')
 
-    # MLP: Blow away Dakota log file here
-    if os.path.exists("dakota_output.txt"):
-        os.remove("dakota_output.txt")
-    dakota_output = open("dakota_output.txt", 'w')
+    # open log file
+    dakota_log_file_name = "Dakota.log"
+    if os.path.exists(dakota_log_file_name):
+        os.remove(dakota_log_file_name)
 
     # remove old output files, if any
     files_to_remove = base_name + ".e"
@@ -38,59 +45,63 @@ if __name__ == "__main__":
         os.remove(file)
 
     # Setup soft link to Peridigm executable so that simulation driver script can find it
+    logfile.write("Creating symbolic link to Peridigm in test directory.\n")
     command = ["ln","-f","-s","../../../../src/Peridigm","."]
     p = Popen(command, stdout=logfile, stderr=logfile)
     return_code = p.wait()
     if return_code != 0:
         result = return_code
+    logfile.write("  Complete (return code " + str(return_code) + ").\n\n")
+    logfile.write("Creating symbolic link to Peridigm in template directory.\n")
     command = ["ln","-f","-s","../../../../../src/Peridigm","./templatedir"]
     p = Popen(command, stdout=logfile, stderr=logfile)
     return_code = p.wait()
     if return_code != 0:
         result = return_code
+    logfile.write("  Complete (return code " + str(return_code) + ").\n\n")
+    logfile.write("Creating symbolic link to exotxt in template directory.\n")
     command = ["ln","-f","-s","../../../../../scripts/exotxt","./templatedir"]
     p = Popen(command, stdout=logfile, stderr=logfile)
     return_code = p.wait()
     if return_code != 0:
         result = return_code
+    logfile.write("  Complete (return code " + str(return_code) + ").\n\n")
 
     # run Dakota
+    logfile.write("Executing Dakota.\n")
     sys.path.append("../../../../src/Peridigm")
     command = ["dakota","-in","dakota_peridigm.in"]
-    p = Popen(command, stdout=dakota_output, stderr=dakota_output)
+    dakota_log_file = open(dakota_log_file_name, 'w')
+    p = Popen(command, stdout=dakota_log_file, stderr=dakota_log_file)
     return_code = p.wait()
+    dakota_log_file.close()
     if return_code != 0:
         result = return_code
+    logfile.write("  Complete (return code " + str(return_code) + ").\n\n")
 
-    # Create results file to parse
-    command = ["grep -A 1 -i \"<<<<< Best parameters\" dakota_output.txt | tail -n 1 | sed 's/^[ \t]*//' | cut -d\" \" -f1 > Dakota_2x1x1.dat"]
-    p = Popen(command, stdout=logfile, stderr=logfile, shell=True)
-    return_code = p.wait()
-    if return_code != 0:
-        result = return_code
+    # extract the modulus that Dakota solved for from the log file
+    computed_bulk_modulus = 0.0
+    dakota_log_file = open(dakota_log_file_name)
+    lines = dakota_log_file.readlines()
+    dakota_log_file.close()
+    for i in range(len(lines)):
+        line = lines[i]
+        if "Best parameters" in line:
+            vals = string.splitfields(line + lines[i+1])
+            for i in range(len(vals)):
+                if "BulkModulus" in vals[i]:
+                    computed_bulk_modulus = float(vals[i-1])
 
-    # compare output files against gold files
-    filename1 = "../"+base_name+"_gold.dat"
-    filename2 = base_name+".dat"
-    f1 = open(filename1, 'r')
-    f2 = open(filename2, 'r')
-    str1 = f1.readline()
-    str2 = f2.readline()
-    f1.close()
-    f2.close()
-    val1 = float(str1)
-    val2 = float(str2)
-    diff = abs((val1 - val2) / val1)
-    logfile.write("diff = "+str(diff))
-    if diff > 1e-8:
+    logfile.write("Computed bulk modulus: " + str(computed_bulk_modulus) + "\n")
+    logfile.write("Truth value: " + str(MODULUS_TRUTH_VALUE) + "\n")
+    rel_error = abs((MODULUS_TRUTH_VALUE - computed_bulk_modulus)/MODULUS_TRUTH_VALUE)
+    logfile.write("Relative error: " + str(rel_error) + "\n")
+    logfile.write("Tolerance: " + str(REL_ERROR_TOLERANCE) + "\n")
+
+    if rel_error > REL_ERROR_TOLERANCE:
        result = -1
-    #command = ["diff "+base_name+".dat "+"../"+base_name+"_gold.dat"]
-    #p = Popen(command, stdout=logfile, stderr=logfile, shell=True)
-    #return_code = p.wait()
-    #if return_code != 0:
-    #    result = return_code
 
-    dakota_output.close()
+    logfile.write("\nTest return code: " + str(result) + "\n\n")
     logfile.close()
 
     # dump the output if the user requested verbose
---
+++ test/verification/Dakota_2x1x1/np1/templatedir/ExtractNodalForces.py
@@ -0,0 +1,64 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import re
+import string
+from subprocess import Popen
+
+if __name__ == "__main__":
+
+    logfile = open("exotxt.log", 'w')
+    command = ["exotxt","peridigm.e","peridigm.txt"]
+    p = Popen(command, stdout=logfile, stderr=logfile)
+    return_code = p.wait()
+    logfile.close()
+
+    text_file = open("peridigm.txt")
+    lines = text_file.readlines()
+    text_file.close()
+
+    data = []
+    flag = False
+    for i in range(len(lines)):
+        line = lines[i]
+        if flag:
+            if "!" in line:
+                flag = False
+            else:
+                data.append(line)
+        if "Nodal variables" in line:
+            data = []
+            flag = True
+
+    values = []
+    for datum in data:
+        vals = string.splitfields(datum)
+        for val in vals:
+            values.append(val)
+
+    num_nodal_variables = 9
+    pt1_vals = values[:num_nodal_variables]
+    pt2_vals = values[num_nodal_variables:2*num_nodal_variables]
+
+    force_index = 3
+    pt1_force = float(pt1_vals[force_index])
+    pt2_force = float(pt2_vals[force_index])
+
+    pt1_truth = -11700000.0
+    pt2_truth =  11700000.0
+
+    error1 = pt1_force - pt1_truth
+    error2 = pt2_force - pt2_truth
+
+    print sys.argv
+
+    if len(sys.argv) != 2:
+       print "\nError:  No output file specified."
+       print "Usage:  ExtractNodalForces.py <output_file>.\n"
+       sys.exit(-1)
+    dakota_file_name = sys.argv[-1]
+    dakota_file = open(dakota_file_name, 'w')
+    dakota_file.write(str(error1) + "\n" + str(error2) + "\n")
+    dakota_file.close()
+    print "\nResults written to", dakota_file_name, "\n"
