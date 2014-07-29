5f2b4d08be2bdddf3c7284d0ebc823eaec7fddf6
David Littlewood <djlittl@sandia.gov>
2014-07-29 10:38:46 -0600

    Incremented version number to 1.3.0 (Dev).

--- src/core/Peridigm_version.h
+++ src/core/Peridigm_version.h
@@ -58,8 +58,8 @@
  * 10.2.4 is designated 100204.  This preserves the comparability of
  * these version numbers with simple comparison operators used in #ifdef tests.
  */
-#define PERIDIGM_MAJOR_MINOR_VERSION 010100
+#define PERIDIGM_MAJOR_MINOR_VERSION 010300
 
-#define PERIDIGM_VERSION_STRING "1.1.0 (Dev)"
+#define PERIDIGM_VERSION_STRING "1.3.0 (Dev)"
 
 #endif // PERIDIGM_VERSION_H
