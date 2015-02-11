00b67d1de0cd20edcc9578a8b66f4cecc714f4e4
David Littlewood <djlittl@sandia.gov>
2015-02-11 16:27:09 -0700

    Damon and Dave added the bond-based elastic constitutive model.

---
+++ src/materials/Peridigm_ElasticBondBasedMaterial.cpp
@@ -0,0 +1,115 @@
+/*! \file Peridigm_ElasticBondBasedMaterial.cpp */
+
+//@HEADER
+// ************************************************************************
+//
+//                             Peridigm
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions?
+// David J. Littlewood   djlittl@sandia.gov
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+
+#include "Peridigm_ElasticBondBasedMaterial.hpp"
+#include "Peridigm_Field.hpp"
+#include "elastic_bond_based.h"
+#include <Teuchos_Assert.hpp>
+
+PeridigmNS::ElasticBondBasedMaterial::ElasticBondBasedMaterial(const Teuchos::ParameterList& params)
+  : Material(params),
+    m_bulkModulus(0.0), m_density(0.0), m_horizon(0.0), m_volumeFieldId(-1), m_damageFieldId(-1),
+    m_modelCoordinatesFieldId(-1), m_coordinatesFieldId(-1), m_forceDensityFieldId(-1), m_bondDamageFieldId(-1)
+{
+  //! \todo Add meaningful asserts on material properties.
+  m_bulkModulus = params.get<double>("Bulk Modulus");
+  m_density = params.get<double>("Density");
+  m_horizon = params.get<double>("Horizon");
+  if(params.isParameter("Young's Modulus") || params.isParameter("Poisson's Ratio") || params.isParameter("Shear Modulus")){
+    TEUCHOS_TEST_FOR_EXCEPT_MSG(true, "**** Error:  The Elastic bond based material model supports only one elastic constant, the bulk modulus.");
+  }
+
+  PeridigmNS::FieldManager& fieldManager = PeridigmNS::FieldManager::self();
+  m_volumeFieldId                  = fieldManager.getFieldId(PeridigmField::ELEMENT, PeridigmField::SCALAR,      PeridigmField::CONSTANT, "Volume");
+  m_damageFieldId                  = fieldManager.getFieldId(PeridigmField::ELEMENT, PeridigmField::SCALAR,      PeridigmField::TWO_STEP, "Damage");
+  m_modelCoordinatesFieldId        = fieldManager.getFieldId(PeridigmField::NODE,    PeridigmField::VECTOR,      PeridigmField::CONSTANT, "Model_Coordinates");
+  m_coordinatesFieldId             = fieldManager.getFieldId(PeridigmField::NODE,    PeridigmField::VECTOR,      PeridigmField::TWO_STEP, "Coordinates");
+  m_forceDensityFieldId            = fieldManager.getFieldId(PeridigmField::NODE,    PeridigmField::VECTOR,      PeridigmField::TWO_STEP, "Force_Density");
+  m_bondDamageFieldId              = fieldManager.getFieldId(PeridigmField::BOND,    PeridigmField::SCALAR,      PeridigmField::TWO_STEP, "Bond_Damage");
+
+  m_fieldIds.push_back(m_volumeFieldId);
+  m_fieldIds.push_back(m_damageFieldId);
+  m_fieldIds.push_back(m_modelCoordinatesFieldId);
+  m_fieldIds.push_back(m_coordinatesFieldId);
+  m_fieldIds.push_back(m_forceDensityFieldId);
+  m_fieldIds.push_back(m_bondDamageFieldId);
+}
+
+PeridigmNS::ElasticBondBasedMaterial::~ElasticBondBasedMaterial()
+{
+}
+
+void
+PeridigmNS::ElasticBondBasedMaterial::initialize(const double dt,
+                                                 const int numOwnedPoints,
+                                                 const int* ownedIDs,
+                                                 const int* neighborhoodList,
+                                                 PeridigmNS::DataManager& dataManager)
+{
+}
+
+void
+PeridigmNS::ElasticBondBasedMaterial::computeForce(const double dt,
+                                          const int numOwnedPoints,
+                                          const int* ownedIDs,
+                                          const int* neighborhoodList,
+                                          PeridigmNS::DataManager& dataManager) const
+{
+  // Zero out the forces
+  dataManager.getData(m_forceDensityFieldId, PeridigmField::STEP_NP1)->PutScalar(0.0);
+
+  // Extract pointers to the underlying data
+  double *x, *y, *cellVolume, *bondDamage, *force;
+
+  dataManager.getData(m_modelCoordinatesFieldId, PeridigmField::STEP_NONE)->ExtractView(&x);
+  dataManager.getData(m_coordinatesFieldId, PeridigmField::STEP_NP1)->ExtractView(&y);
+  dataManager.getData(m_volumeFieldId, PeridigmField::STEP_NONE)->ExtractView(&cellVolume);
+  dataManager.getData(m_bondDamageFieldId, PeridigmField::STEP_NP1)->ExtractView(&bondDamage);
+  dataManager.getData(m_forceDensityFieldId, PeridigmField::STEP_NP1)->ExtractView(&force);
+
+  MATERIAL_EVALUATION::computeInternalForceElasticBondBased(x,y,cellVolume,bondDamage,force,neighborhoodList,numOwnedPoints,m_bulkModulus,m_horizon);
+}
---
+++ src/materials/elastic_bond_based.cxx
@@ -0,0 +1,149 @@
+//! \file elastic_bond_based.cxx
+
+//@HEADER
+// ************************************************************************
+//
+//                             Peridigm
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions?
+// David J. Littlewood   djlittl@sandia.gov
+// John A. Mitchell      jamitch@sandia.gov
+// Michael L. Parks      mlparks@sandia.gov
+// Stewart A. Silling    sasilli@sandia.gov
+//
+// ************************************************************************
+//@HEADER
+
+#include <cmath>
+#include <Sacado.hpp>
+#include <boost/math/constants/constants.hpp>
+#include "elastic_bond_based.h"
+#include "material_utilities.h"
+
+namespace MATERIAL_EVALUATION {
+
+template<typename ScalarT>
+void computeInternalForceElasticBondBased
+(
+		const double* xOverlap,
+		const ScalarT* yOverlap,
+		const double* volumeOverlap,
+		const double* bondDamage,
+		ScalarT* fInternalOverlap,
+		const int* localNeighborList,
+		int numOwnedPoints,
+		double BULK_MODULUS,
+        double horizon
+)
+{
+  double volume, neighborVolume, X[3], neighborX[3], initialBondLength, damageOnBond;
+  ScalarT Y[3], neighborY[3], currentBondLength, stretch, t, fx, fy, fz;
+  int neighborhoodIndex(0), bondDamageIndex(0), neighborId;
+
+  const double pi = boost::math::constants::pi<double>();
+  double constant = 18.0*BULK_MODULUS/(pi*horizon*horizon*horizon*horizon);
+
+  for(int p=0 ; p<numOwnedPoints ; p++){
+
+    X[0] = xOverlap[p*3];
+    X[1] = xOverlap[p*3+1];
+    X[2] = xOverlap[p*3+2];
+    Y[0] = yOverlap[p*3];
+    Y[1] = yOverlap[p*3+1];
+    Y[2] = yOverlap[p*3+2];
+    volume = volumeOverlap[p];
+
+    int numNeighbors = localNeighborList[neighborhoodIndex++];
+	for(int n=0; n<numNeighbors; n++){
+
+      neighborId = localNeighborList[neighborhoodIndex++];
+      neighborX[0] = xOverlap[neighborId*3];
+      neighborX[1] = xOverlap[neighborId*3+1];
+      neighborX[2] = xOverlap[neighborId*3+2];
+      neighborY[0] = yOverlap[neighborId*3];
+      neighborY[1] = yOverlap[neighborId*3+1];
+      neighborY[2] = yOverlap[neighborId*3+2];
+      neighborVolume = volumeOverlap[neighborId];
+      
+      initialBondLength = std::sqrt( (neighborX[0]-X[0])*(neighborX[0]-X[0]) + (neighborX[1]-X[1])*(neighborX[1]-X[1]) + (neighborX[2]-X[2])*(neighborX[2]-X[2]) );
+      currentBondLength = std::sqrt( (neighborY[0]-Y[0])*(neighborY[0]-Y[0]) + (neighborY[1]-Y[1])*(neighborY[1]-Y[1]) + (neighborY[2]-Y[2])*(neighborY[2]-Y[2]) );
+      stretch = (currentBondLength - initialBondLength)/initialBondLength;
+
+      damageOnBond = bondDamage[bondDamageIndex++];
+
+      t = 0.5*(1.0 - damageOnBond)*stretch*constant;
+
+      fx = t * (neighborY[0] - Y[0]) / currentBondLength;
+      fy = t * (neighborY[1] - Y[1]) / currentBondLength;
+      fz = t * (neighborY[2] - Y[2]) / currentBondLength;
+
+      fInternalOverlap[3*p+0] += fx*neighborVolume;
+      fInternalOverlap[3*p+1] += fy*neighborVolume;
+      fInternalOverlap[3*p+2] += fz*neighborVolume;
+      fInternalOverlap[3*neighborId+0] -= fx*volume;
+      fInternalOverlap[3*neighborId+1] -= fy*volume;
+      fInternalOverlap[3*neighborId+2] -= fz*volume;
+    }
+  }
+}
+
+/** Explicit template instantiation for double. */
+template void computeInternalForceElasticBondBased<double>
+(
+		const double* xOverlap,
+		const double* yOverlap,
+		const double* volumeOverlap,
+		const double* bondDamage,
+		double* fInternalOverlap,
+		const int*  localNeighborList,
+		int numOwnedPoints,
+		double BULK_MODULUS,
+        double horizon
+ );
+
+/** Explicit template instantiation for Sacado::Fad::DFad<double>. */
+template void computeInternalForceElasticBondBased<Sacado::Fad::DFad<double> >
+(
+		const double* xOverlap,
+		const Sacado::Fad::DFad<double>* yOverlap,
+		const double* volumeOverlap,
+		const double* bondDamage,
+		Sacado::Fad::DFad<double>* fInternalOverlap,
+		const int*  localNeighborList,
+		int numOwnedPoints,
+		double BULK_MODULUS,
+        double horizon
+);
+
+}
