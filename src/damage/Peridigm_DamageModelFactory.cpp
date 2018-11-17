be9558be6c563528807800af1387b3c5793e370e
andris.freimanis <andris.freimanis_1@rtu.lv>
2018-11-17 12:17:31 +0200

    Added a fatigue damage model and tests.

--- src/damage/Peridigm_DamageModelFactory.cpp
+++ src/damage/Peridigm_DamageModelFactory.cpp
@@ -50,6 +50,7 @@
 #include "Peridigm_CriticalStretchDamageModel.hpp"
 #include "Peridigm_UserDefinedTimeDependentCriticalStretchDamageModel.hpp"
 #include "Peridigm_InterfaceAwareDamageModel.hpp"
+#include "Peridigm_FatigueDamageModel.hpp"
 
 using namespace std;
 
@@ -65,11 +66,13 @@ PeridigmNS::DamageModelFactory::create(const Teuchos::ParameterList& damageModel
   else if(damageModelName == "Interface Aware")
     damageModel = Teuchos::rcp( new InterfaceAwareDamageModel(damageModelParams) );
   else if(damageModelName == "Time Dependent Critical Stretch")
-    damageModel = Teuchos::rcp( new UserDefinedTimeDependentCriticalStretchDamageModel(damageModelParams) );  
+    damageModel = Teuchos::rcp( new UserDefinedTimeDependentCriticalStretchDamageModel(damageModelParams) );   
+  else if(damageModelName == "Fatigue")
+    damageModel = Teuchos::rcp( new FatigueDamageModel(damageModelParams) );  
   else {
     string invalidDamageModel("\n**** Unrecognized damage model type: ");
     invalidDamageModel += damageModelName;
-    invalidDamageModel += ", must be \"Critical Stretch\", \"Time Dependent Critical Stretch\" or \"Interface Aware\".\n";
+    invalidDamageModel += ", must be \"Critical Stretch\", \"Time Dependent Critical Stretch\", \"Interface Aware\" or \"Fatigue\".\n";
     TEUCHOS_TEST_FOR_EXCEPT_MSG(true, invalidDamageModel);
   }
   
