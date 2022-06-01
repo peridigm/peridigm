b3a81805248f0807bb2b49962f72f177e369fc94
John T. Foster <johntfosterjr@gmail.com>
2022-06-01 08:28:29 -0500

    Last overscribe flag

--- test/verification/InterfaceAwareDamageModel/np3/InterfaceAwareDamageModelTwoBlocksDifferentValues.py
+++ test/verification/InterfaceAwareDamageModel/np3/InterfaceAwareDamageModelTwoBlocksDifferentValues.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
             os.remove(file)
 
     # run Peridigm
-    command = ["mpirun", "-n", "3", "../../../../src/Peridigm", "../"+base_name+".yaml"]
+    command = ["mpirun", "-n", "3", "--oversubscribe", "../../../../src/Peridigm", "../"+base_name+".yaml"]
     p = Popen(command, stdout=logfile, stderr=logfile)
     return_code = p.wait()
     if return_code != 0:
